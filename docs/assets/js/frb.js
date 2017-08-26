97e2b091ceedec3a1af4dc68b61b366a2de8540d
Justin Johnson <justin.d.johnson@gmail.com>
2017-08-25 22:31:25 -0600

    Small updates to styles to improve transitions

--- docs/assets/js/frb.js
+++ docs/assets/js/frb.js
@@ -3,8 +3,10 @@ var frb = (function(jQuery) {
     var a = {};
 
     a.init = function () {
+        $(this).children("ul").hide("fast");
+
         $("li.top_nav").click(function () {
-            $(this).children("ul").toggle("slow");
+            $(this).children("ul").slideToggle("slow");
         });
     }
 
