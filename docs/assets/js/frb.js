9f2444353609161bd2f15aec33eca8789ce86986
Justin Johnson <jjohnson@westedge.us>
2017-08-28 10:37:26 -0600

    Possible fix for click toggling issues, cleaned up some styles and commented JS

--- docs/assets/js/frb.js
+++ docs/assets/js/frb.js
@@ -1,18 +1,31 @@
+// FRB "app" provides page interactivity
 var frb = (function(jQuery) {
 
+    // create an app container
     var a = {};
 
+    // init function binds events to the DOM
     a.init = function () {
-        $("li.top_nav").children("ul").slideToggle("fast");
 
-        $("li.top_nav").click(function () {
-            $(this).children("ul").slideToggle("slow");
+        $("p.list_title").click(function () {
+            // get the parent "li" object
+            $(this).parent()
+
+            // get all ULs belonging to parent li
+            .children("ul")
+            
+            // toggle uls to the opposite state
+            .slideToggle("slow");
         });
     }
 
+    // return the container
     return a;
 }($));
 
+
+
+// Bootstrap the FRB app once page has loaded
 $(function () {
     frb.init();
 });
