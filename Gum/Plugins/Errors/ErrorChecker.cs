524e3dedbf6a0a3a9a505934f275bbe2e85b6616
Victor Chelaru <VicChelaru@gmail.com>
2018-01-02 21:51:57 -0700

    Created new BehaviorInstanceSave to allow instances in behaviors to themselves speicify needed behaviors.

--- Gum/Plugins/Errors/ErrorChecker.cs
+++ Gum/Plugins/Errors/ErrorChecker.cs
@@ -1,4 +1,5 @@
 ﻿using Gum.DataTypes;
+using Gum.Managers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -60,12 +61,44 @@ namespace Gum.Plugins.Errors
                     candidateInstances = candidateInstances.Where(item => item.IsOfType(behaviorInstance.BaseType));
                 }
 
+                if (behaviorInstance.Behaviors.Any())
+                {
+                    var requiredBehaviorNames = behaviorInstance.Behaviors.Select(item => item.Name);
+                    candidateInstances = candidateInstances.Where(item =>
+                    {
+                        bool fulfillsRequirements = false;
+                        var element = ObjectFinder.Self.GetComponent(item.BaseType);
+                        if (element != null)
+                        {
+                            var implementedBehaviorNames = element.Behaviors.Select(implementedBehavior => implementedBehavior.BehaviorName);
+
+                            fulfillsRequirements = requiredBehaviorNames.All(required => implementedBehaviorNames.Contains(required));
+
+                        }
+                        return fulfillsRequirements;
+                    });
+
+                }
+
                 if (!candidateInstances.Any())
                 {
                     string message = $"Missing instance with name {behaviorInstance.Name}";
                     if (!string.IsNullOrEmpty(behaviorInstance.BaseType))
                     {
                         message += $" of type {behaviorInstance.BaseType}";
+                    }
+                    if(behaviorInstance.Behaviors.Any())
+                    {
+                        if(behaviorInstance.Behaviors.Count == 1)
+                        {
+                            message += " with behavior type";
+                        }
+                        else
+                        {
+                            message += " with behavior types";
+                        }
+                        var behaviorsJoined = string.Join(", ", behaviorInstance.Behaviors.Select(item => item.Name).ToArray());
+                        message += behaviorsJoined;
                     }
 
                     message += $" needed by behavior {behavior.Name}";
