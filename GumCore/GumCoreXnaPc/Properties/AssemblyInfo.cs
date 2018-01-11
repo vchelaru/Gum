fead6c92fd3385133fa22b99828cf112d21cb2e2
bicheichane <bernardopinho@gmail.com>
2018-01-11 19:20:58 +0000

    GumCoreAndroid fix

--- GumCore/GumCoreXnaPc/Properties/AssemblyInfo.cs
+++ GumCore/GumCoreXnaPc/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GumCoreXnaPc")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright �  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
