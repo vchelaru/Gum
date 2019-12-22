eae1b9f83a5d566b3a9b6413be8aed6632eac8af
Victor Chelaru <VicChelaru@gmail.com>
2019-12-22 16:13:29 -0700

    Added LayerCameraSettings docs.

--- RenderingLibrary/Graphics/LayerCameraSettings.cs
+++ RenderingLibrary/Graphics/LayerCameraSettings.cs
@@ -7,6 +7,10 @@ namespace RenderingLibrary.Graphics
 {
     public class LayerCameraSettings
     {
+        /// <summary>
+        /// Whether the current layer is in Screen Space. If true, then the Camera position does
+        /// not impact the position of objects on this layer.
+        /// </summary>
         public bool IsInScreenSpace
         {
             get;
