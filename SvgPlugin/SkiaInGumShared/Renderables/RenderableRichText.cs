f27a87d69529ac340660c9b2575b2f4d2cb54210
Victor Chelaru <VicChelaru@gmail.com>
2020-02-24 09:06:56 -0700

    Added rich text kit and started adding RenderableRichTextBox.

---
+++ SvgPlugin/SkiaInGumShared/Renderables/RenderableRichText.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkiaPlugin.Renderables
+{
+    public class RenderableRichText : RenderableSkiaObject
+    {
+        internal override void DrawToSurface(SKSurface surface)
+        {
+            // throw new notimplemented
+        }
+    }
+}
