2d57827b7b30c22e2d4159c2ba7cdfb535f25ec7
Victor Chelaru <VicChelaru@gmail.com>
2020-12-17 17:03:31 -0700

    Windows no longer appear off the bottom of the screen.

--- CommonFormsAndControls/FormUtilities.cs
+++ CommonFormsAndControls/FormUtilities.cs
@@ -33,10 +33,26 @@ namespace CommonFormsAndControls
 
         public void PositionCenterToCursor<T>(T instance) where T : Form
         {
-
             instance.StartPosition = FormStartPosition.Manual;
-            instance.Location = new Point(Form.MousePosition.X - instance.Width/2, 
-                Form.MousePosition.Y - instance.Height/2);
+
+            Form myForm;
+            Screen myScreen = Screen.FromControl(instance);
+            Rectangle area = myScreen.WorkingArea;
+
+            const int borderBuffer = 80;
+            var screenBottom = area.Height - borderBuffer; // On Vic's device this is inaccurate, it's a little too big. Leave room for bar and give it a buffer.
+
+            var desiredX = Form.MousePosition.X - instance.Width / 2;
+
+            var desiredY = Form.MousePosition.Y - instance.Height / 2;
+            var bottomOverlap = (desiredY + instance.Height / 2) - screenBottom;
+
+            if(bottomOverlap > 0)
+            {
+                desiredY -= bottomOverlap;
+            }
+
+            instance.Location = new Point(desiredX, desiredY);
 
         }
     }
