7eb3eb11e2d7e66c208587b3473acf4f32574776
Victor Chelaru <VicChelaru@gmail.com>
2021-01-26 14:49:20 -0700

    Moved IsPointInside to GUE since it's needed by skia

--- GumRuntime/GraphicalUiElement.cs
+++ GumRuntime/GraphicalUiElement.cs
@@ -5149,6 +5149,23 @@ namespace Gum.Wireframe
 #endif
         #endregion
 
+
+#if SKIA
+        public bool IsPointInside(float x, float y)
+        {
+            var asIpso = this as IRenderableIpso;
+
+            var absoluteX = asIpso.GetAbsoluteX();
+            var absoluteY = asIpso.GetAbsoluteY();
+
+            return
+                x > absoluteX &&
+                y > absoluteY &&
+                x < absoluteX + this.GetAbsoluteWidth() &&
+                y < absoluteY + this.GetAbsoluteHeight();
+        }
+#endif
+
         #endregion
     }
 }
