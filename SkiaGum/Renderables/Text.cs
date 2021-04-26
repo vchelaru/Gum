f214a24c1d216cd5c1544807ef96bfbfca234b67
Victor Chelaru <VicChelaru@gmail.com>
2021-04-26 12:37:55 -0600

    Removed unnecessary XamarinForms reference in SkiaGum.

--- SkiaGum/Renderables/Text.cs
+++ SkiaGum/Renderables/Text.cs
@@ -9,7 +9,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
 using Topten.RichTextKit;
-using Xamarin.Forms.PlatformConfiguration.TizenSpecific;
 
 namespace SkiaGum
 {
