bb1bf0fbddd62f3fa754cef8c7082f913fe68d0f
Victor Chelaru <VicChelaru@gmail.com>
2022-06-11 16:45:29 -0600

    Fixed rotated display lines.

--- Gum/Wireframe/Editors/DimensionDisplay.cs
+++ Gum/Wireframe/Editors/DimensionDisplay.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using RenderingLibrary;
 using RenderingLibrary.Graphics;
+using RenderingLibrary.Math;
 using RenderingLibrary.Math.Geometry;
 using System;
 using System.Collections.Generic;
@@ -28,7 +29,7 @@ namespace Gum.Wireframe.Editors
         Line endCap2;
 
         Line middleLine;
-        Text dimensionDisplay;
+        Text dimensionDisplayText;
 
         float Zoom => systemManagers.Renderer.Camera.Zoom;
 
@@ -43,12 +44,12 @@ namespace Gum.Wireframe.Editors
             middleLine = new Line(systemManagers);
             AddLineToManagers(middleLine);
 
-            dimensionDisplay = new Text(systemManagers);
-            dimensionDisplay.RenderBoundary = false;
-            dimensionDisplay.Width = 0;
-            dimensionDisplay.Height = 0;
+            dimensionDisplayText = new Text(systemManagers);
+            dimensionDisplayText.RenderBoundary = false;
+            dimensionDisplayText.Width = 0;
+            dimensionDisplayText.Height = 0;
 
-            systemManagers.TextManager.Add(dimensionDisplay);
+            systemManagers.TextManager.Add(dimensionDisplayText);
             this.systemManagers = systemManagers;
 
             endCap1 = new Line(systemManagers);
@@ -64,12 +65,12 @@ namespace Gum.Wireframe.Editors
             endCap2.Color = color;
 
             middleLine.Color = color;
-            dimensionDisplay.Color = color;
+            dimensionDisplayText.Color = color;
         }
 
         public void SetVisible(bool isVisible)
         {
-            endCap1.Visible = endCap2.Visible = middleLine.Visible = dimensionDisplay.Visible = isVisible;
+            endCap1.Visible = endCap2.Visible = middleLine.Visible = dimensionDisplayText.Visible = isVisible;
         }
 
         public void Destroy()
@@ -79,7 +80,7 @@ namespace Gum.Wireframe.Editors
 
             systemManagers.ShapeManager.Remove(middleLine);
 
-            systemManagers.TextManager.Remove(dimensionDisplay);
+            systemManagers.TextManager.Remove(dimensionDisplayText);
 
         }
 
@@ -93,9 +94,18 @@ namespace Gum.Wireframe.Editors
             var top = asIpso.GetAbsoluteY();
             var absoluteHeight = asIpso.Height;
 
+            var topLeft = new Vector2(left, top);
+
             float fromBodyOffset = 26 / Zoom;
             float endCapLength = 12 / Zoom;
-            dimensionDisplay.FontScale = 1/Zoom;
+            dimensionDisplayText.FontScale = 1/Zoom;
+
+            var rotationMatrix = asIpso.GetAbsoluteRotationMatrix();
+            var rotatedLeftDirection = new Vector2(rotationMatrix.Left.X, rotationMatrix.Left.Y);
+            var rotatedRightDirection = new Vector2(rotationMatrix.Right.X, rotationMatrix.Right.Y);
+            var rotatedUpDirection = new Vector2(rotationMatrix.Down.X, rotationMatrix.Down.Y);
+            var rotatedDownDirection = new Vector2(rotationMatrix.Up.X, rotationMatrix.Up.Y);
+            var extraTextOffset = 4;
 
             if(widthOrHeight == WidthOrHeight.Width)
             {
@@ -125,38 +135,34 @@ namespace Gum.Wireframe.Editors
                         break;
                 }
 
-                middleLine.X = left;
-                middleLine.RelativePoint.X = absoluteWidth;
-
-                middleLine.Y = top - fromBodyOffset;
-                middleLine.RelativePoint.Y = 0;
+                middleLine.SetPosition(topLeft + rotatedUpDirection * fromBodyOffset);
+                middleLine.RelativePoint = rotatedRightDirection * absoluteWidth;
 
                 if(suffix != null)
                 {
-                    dimensionDisplay.RawText = $"{objectToUpdateTo.Width:0.0}{suffix}\n({absoluteWidth:0.0} px)"; 
+                    dimensionDisplayText.RawText = $"{objectToUpdateTo.Width:0.0}{suffix}\n({absoluteWidth:0.0} px)"; 
 
                 }
                 else
                 {
-                    dimensionDisplay.RawText = absoluteWidth.ToString("0.0");
+                    dimensionDisplayText.RawText = absoluteWidth.ToString("0.0");
                 }
 
-                dimensionDisplay.X = middleLine.X + absoluteWidth / 2.0f - dimensionDisplay.WrappedTextWidth / 2.0f;
-                dimensionDisplay.Y = top - fromBodyOffset - dimensionDisplay.WrappedTextHeight;
-                dimensionDisplay.HorizontalAlignment = HorizontalAlignment.Center;
-                dimensionDisplay.VerticalAlignment = VerticalAlignment.Bottom;
+                dimensionDisplayText.SetPosition(
+                    topLeft + 
+                    rotatedUpDirection * (fromBodyOffset + extraTextOffset + dimensionDisplayText.WrappedTextHeight) + 
+                    rotatedRightDirection * (-dimensionDisplayText.WrappedTextWidth/2 + absoluteWidth/2)  );
+                //dimensionDisplayText.X = middleLine.X + absoluteWidth / 2.0f - dimensionDisplayText.WrappedTextWidth / 2.0f;
+                //dimensionDisplayText.Y = top - fromBodyOffset - dimensionDisplayText.WrappedTextHeight;
+                dimensionDisplayText.HorizontalAlignment = HorizontalAlignment.Center;
+                dimensionDisplayText.VerticalAlignment = VerticalAlignment.Center;
 
-                endCap1.X = left;
-                endCap1.Y = middleLine.Y - endCapLength/2.0f;
+                endCap1.SetPosition(middleLine.GetPosition() + rotatedUpDirection * endCapLength / 2.0f);
+                endCap1.RelativePoint = rotatedDownDirection * endCapLength;
 
-                endCap1.RelativePoint.X = 0;
-                endCap1.RelativePoint.Y = endCapLength;
+                endCap2.SetPosition(middleLine.GetPosition() + rotatedRightDirection * absoluteWidth + rotatedUpDirection * endCapLength / 2.0f);
+                endCap2.RelativePoint = rotatedDownDirection * endCapLength;
 
-                endCap2.X = left + absoluteWidth;
-                endCap2.Y = endCap1.Y;
-
-                endCap2.RelativePoint.X = 0;
-                endCap2.RelativePoint.Y = endCapLength;
             }
             else // height
             {
@@ -186,37 +192,33 @@ namespace Gum.Wireframe.Editors
                         break;
                 }
 
+                // up is 0,1,0, which is actually down for Gum. Confusing, I know, but this results in the correct math
+                middleLine.X = (topLeft + rotatedLeftDirection * fromBodyOffset).X;
+                middleLine.Y = (topLeft + rotatedLeftDirection * fromBodyOffset).Y;
 
-                middleLine.X = left - fromBodyOffset;
-                middleLine.RelativePoint.X = 0;
-
-                middleLine.Y = top;
-                middleLine.RelativePoint.Y = absoluteHeight;
+                middleLine.RelativePoint = rotatedDownDirection * absoluteHeight;
 
                 if (suffix != null)
                 {
-                    dimensionDisplay.RawText = $"{objectToUpdateTo.Height:0.0}{suffix}\n({absoluteHeight:0.0} px)";
+                    dimensionDisplayText.RawText = $"{objectToUpdateTo.Height:0.0}{suffix}\n({absoluteHeight:0.0} px)";
 
                 }
                 else
                 {
-                    dimensionDisplay.RawText = absoluteHeight.ToString("0.0");
+                    dimensionDisplayText.RawText = absoluteHeight.ToString("0.0");
                 }
-                dimensionDisplay.X = middleLine.X - 5 - dimensionDisplay.WrappedTextWidth;
-                dimensionDisplay.Y = top + absoluteHeight / 2.0f - dimensionDisplay.WrappedTextHeight / 2.0f;
-                dimensionDisplay.VerticalAlignment = VerticalAlignment.Center;
 
-                endCap1.X = middleLine.X - endCapLength / 2.0f;
-                endCap1.RelativePoint.X = endCapLength;
+                var dimensionDisplayPosition = topLeft + rotatedDownDirection * .5f * absoluteHeight + rotatedLeftDirection * (fromBodyOffset + extraTextOffset);
 
-                endCap1.Y = top;
-                endCap1.RelativePoint.Y = 0;
+                dimensionDisplayText.X = dimensionDisplayPosition.X - dimensionDisplayText.WrappedTextWidth;
+                dimensionDisplayText.Y = dimensionDisplayPosition.Y - dimensionDisplayText.WrappedTextHeight / 2.0f;
+                dimensionDisplayText.VerticalAlignment = VerticalAlignment.Center;
 
-                endCap2.X = endCap1.X;
-                endCap2.RelativePoint.X = endCap1.RelativePoint.X;
+                endCap1.SetPosition(middleLine.GetPosition() + rotatedLeftDirection * endCapLength / 2.0f);
+                endCap1.RelativePoint = rotatedRightDirection * endCapLength;
 
-                endCap2.Y = top + absoluteHeight;
-                endCap2.RelativePoint.Y = 0;
+                endCap2.SetPosition(middleLine.GetPosition() + middleLine.RelativePoint + rotatedLeftDirection * endCapLength / 2.0f);
+                endCap2.RelativePoint = rotatedRightDirection * endCapLength;
             }
 
 
