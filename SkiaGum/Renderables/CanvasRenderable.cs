8b7b9db7846d6461d98dd3b55c0d1b55cb6e3701
Victor Chelaru <VicChelaru@gmail.com>
2023-05-16 08:55:19 -0600

    Added CanvasRenderable

---
+++ SkiaGum/Renderables/CanvasRenderable.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.Xna.Framework;
+using RenderingLibrary;
+using RenderingLibrary.Graphics;
+using SkiaGum.GueDeriving;
+using SkiaSharp.Skottie;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace SkiaGum.Renderables
+{
+    internal class CanvasRenderable : IRenderableIpso, IVisible
+    {
+        public object Tag { get; set; }
+        public string Name
+        {
+            get;
+            set;
+        }
+
+
+        ObservableCollection<IRenderableIpso> mChildren;
+        public ObservableCollection<IRenderableIpso> Children
+        {
+            get { return mChildren; }
+        }
+
+        IRenderableIpso mParent;
+        public IRenderableIpso Parent
+        {
+            get { return mParent; }
+            set
+            {
+                if (mParent != value)
+                {
+                    if (mParent != null)
+                    {
+                        mParent.Children.Remove(this);
+                    }
+                    mParent = value;
+                    if (mParent != null)
+                    {
+                        mParent.Children.Add(this);
+                    }
+                }
+            }
+        }
+
+        public ColorOperation ColorOperation { get; set; } = ColorOperation.Modulate;
+
+        Vector2 Position;
+
+        public float X
+        {
+            get { return Position.X; }
+            set { Position.X = value; }
+        }
+
+        public float Y
+        {
+            get { return Position.Y; }
+            set { Position.Y = value; }
+        }
+
+        public float Z
+        {
+            get;
+            set;
+        }
+
+        public float Width
+        {
+            get;
+            set;
+        }
+
+        public float Height
+        {
+            get;
+            set;
+        }
+
+        public float Rotation { get; set; }
+
+
+        public bool FlipHorizontal
+        {
+            get;
+            set;
+        }
+
+        public bool FlipVertical
+        {
+            get;
+            set;
+        }
+
+        void IRenderableIpso.SetParentDirect(IRenderableIpso parent) => mParent = parent;
+
+        public bool Wrap => false;
+
+        public event Action<SkiaSharp.SKCanvas> CustomRender;
+
+        public CanvasRenderable()
+        {
+            mChildren = new ObservableCollection<IRenderableIpso>();
+
+            Visible = true;
+            Width = 100;
+            Height = 100;
+        }
+
+
+        public void PreRender() { }
+
+#if SKIA
+        public void Render(SkiaSharp.SKCanvas canvas)
+        {
+            if (AbsoluteVisible)
+            {
+                var absoluteX = this.GetAbsoluteX();
+                var absoluteY = this.GetAbsoluteY();
+                var boundingRect = new SkiaSharp.SKRect(absoluteX, absoluteY, absoluteX + this.Width, absoluteY + this.Height);
+
+                // offset by the X/Y so that everything is relative to this absolute:
+                canvas.Save();
+
+                canvas.Translate(absoluteX, absoluteY);
+
+                CustomRender?.Invoke(canvas);
+
+                canvas.Restore();
+            }
+        }
+#else
+        public bool ClipsChildren { get; set; }
+        public Microsoft.Xna.Framework.Graphics.BlendState BlendState
+        {
+            get
+            {
+                return Microsoft.Xna.Framework.Graphics.BlendState.AlphaBlend; //?
+
+            }
+
+        }
+        public void Render(SpriteRenderer spriteRenderer, SystemManagers managers)
+        {
+            // todo
+        }
+
+
+#endif
+
+
+        #region IVisible Implementation
+
+        public bool Visible
+        {
+            get;
+            set;
+        }
+
+        public bool AbsoluteVisible
+        {
+            get
+            {
+                if (((IVisible)this).Parent == null)
+                {
+                    return Visible;
+                }
+                else
+                {
+                    return Visible && ((IVisible)this).Parent.AbsoluteVisible;
+                }
+            }
+        }
+
+        IVisible IVisible.Parent
+        {
+            get
+            {
+                return ((IRenderableIpso)this).Parent as IVisible;
+            }
+        }
+
+        #endregion
+
+
+
+    }
+}
