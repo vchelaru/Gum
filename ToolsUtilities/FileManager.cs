ba8f02c74f79c390529e4eb144b980edfbde42fb
Edu Garcia <28616+Arcnor@users.noreply.github.com>
2023-10-15 11:41:28 +0100

    Remove unused imports (ToolsUtilities)

--- ToolsUtilities/FileManager.cs
+++ ToolsUtilities/FileManager.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Reflection;
 using System.IO;
 using System.Xml.Serialization;
 using System.Xml;
-using System.Runtime.Serialization;
-using System.ComponentModel;
 
 namespace ToolsUtilities
 {
