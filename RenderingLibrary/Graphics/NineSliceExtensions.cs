cfbf549d54f8e65982cdec63d7f435231e344321
Victor Chelaru <VicChelaru@gmail.com>
2023-10-28 08:20:58 -0600

    More fixes to get FRB working with GumCommon

--- RenderingLibrary/Graphics/NineSliceExtensions.cs
+++ RenderingLibrary/Graphics/NineSliceExtensions.cs
@@ -1,142 +1,141 @@
 ﻿using System;
 
-namespace RenderingLibrary.Graphics
+namespace RenderingLibrary.Graphics;
+
+public enum NineSliceSections
 {
-    public enum NineSliceSections
+    TopLeft,
+    Top,
+    TopRight,
+    Left,
+    Center,
+    Right,
+    BottomLeft,
+    Bottom,
+    BottomRight
+}
+
+public static class NineSliceExtensions
+{
+    public static string[] PossibleNineSliceEndings
     {
-        TopLeft,
-        Top,
-        TopRight,
-        Left,
-        Center,
-        Right,
-        BottomLeft,
-        Bottom,
-        BottomRight
+        get;
+        private set;
     }
 
-    public static class NineSliceExtensions
+    static NineSliceExtensions()
     {
-        public static string[] PossibleNineSliceEndings
-        {
-            get;
-            private set;
-        }
+        PossibleNineSliceEndings = new string[9];
+        PossibleNineSliceEndings[(int)NineSliceSections.Center] = "_center";
+        PossibleNineSliceEndings[(int)NineSliceSections.Left] = "_left";
+        PossibleNineSliceEndings[(int)NineSliceSections.Right] = "_right";
+        PossibleNineSliceEndings[(int)NineSliceSections.TopLeft] = "_topLeft";
+        PossibleNineSliceEndings[(int)NineSliceSections.Top] = "_topCenter";
+        PossibleNineSliceEndings[(int)NineSliceSections.TopRight] = "_topRight";
+        PossibleNineSliceEndings[(int)NineSliceSections.BottomLeft] = "_bottomLeft";
+        PossibleNineSliceEndings[(int)NineSliceSections.Bottom] = "_bottomCenter";
+        PossibleNineSliceEndings[(int)NineSliceSections.BottomRight] = "_bottomRight";
+    }
 
-        static NineSliceExtensions()
-        {
-            PossibleNineSliceEndings = new string[9];
-            PossibleNineSliceEndings[(int)NineSliceSections.Center] = "_center";
-            PossibleNineSliceEndings[(int)NineSliceSections.Left] = "_left";
-            PossibleNineSliceEndings[(int)NineSliceSections.Right] = "_right";
-            PossibleNineSliceEndings[(int)NineSliceSections.TopLeft] = "_topLeft";
-            PossibleNineSliceEndings[(int)NineSliceSections.Top] = "_topCenter";
-            PossibleNineSliceEndings[(int)NineSliceSections.TopRight] = "_topRight";
-            PossibleNineSliceEndings[(int)NineSliceSections.BottomLeft] = "_bottomLeft";
-            PossibleNineSliceEndings[(int)NineSliceSections.Bottom] = "_bottomCenter";
-            PossibleNineSliceEndings[(int)NineSliceSections.BottomRight] = "_bottomRight";
-        }
+    public static bool GetIfShouldUsePattern(string absoluteTexture)
+    {
+        bool usePattern = false;
 
-        public static bool GetIfShouldUsePattern(string absoluteTexture)
+        string withoutExtension = RemoveExtension(absoluteTexture);
+        foreach (var kvp in PossibleNineSliceEndings)
         {
-            bool usePattern = false;
-
-            string withoutExtension = RemoveExtension(absoluteTexture);
-            foreach (var kvp in PossibleNineSliceEndings)
+            if (withoutExtension.EndsWith(kvp, StringComparison.OrdinalIgnoreCase))
             {
-                if (withoutExtension.EndsWith(kvp, StringComparison.OrdinalIgnoreCase))
-                {
-                    usePattern = true;
-                    break;
-                }
+                usePattern = true;
+                break;
             }
-            return usePattern;
+        }
+        return usePattern;
+    }
+
+
+
+    public static string GetBareTextureForNineSliceTexture(string absoluteTexture)
+    {
+        string extension = GetExtension(absoluteTexture);
+
+        string withoutExtension = RemoveExtension(absoluteTexture);
+
+        string toReturn = withoutExtension;
+
+        foreach (var kvp in PossibleNineSliceEndings)
+        {
+            if (withoutExtension.EndsWith(kvp, StringComparison.OrdinalIgnoreCase))
+            {
+                toReturn = withoutExtension.Substring(0, withoutExtension.Length - kvp.Length);
+                break;
+            }
         }
 
+        // No extensions, because we'll need to append that
+        //toReturn += "." + extension;
 
+        return toReturn;
+    }
 
-        public static string GetBareTextureForNineSliceTexture(string absoluteTexture)
+    // local method for portability:
+    static string RemoveExtension(string fileName)
+    {
+        int extensionLength = GetExtension(fileName).Length;
+
+        if (extensionLength == 0)
+            return fileName;
+
+        if (fileName.Length > extensionLength && fileName[fileName.Length - (extensionLength + 1)] == '.')
+            return fileName.Substring(0, fileName.Length - (extensionLength + 1));
+        else
+            return fileName;
+    }
+
+    static string GetExtension(string fileName)
+    {
+        try
         {
-            string extension = GetExtension(absoluteTexture);
-
-            string withoutExtension = RemoveExtension(absoluteTexture);
-
-            string toReturn = withoutExtension;
-
-            foreach (var kvp in PossibleNineSliceEndings)
+            if (fileName == null)
             {
-                if (withoutExtension.EndsWith(kvp, StringComparison.OrdinalIgnoreCase))
-                {
-                    toReturn = withoutExtension.Substring(0, withoutExtension.Length - kvp.Length);
-                    break;
-                }
+                return "";
             }
 
-            // No extensions, because we'll need to append that
-            //toReturn += "." + extension;
 
-            return toReturn;
-        }
-
-        // local method for portability:
-        static string RemoveExtension(string fileName)
-        {
-            int extensionLength = GetExtension(fileName).Length;
-
-            if (extensionLength == 0)
-                return fileName;
-
-            if (fileName.Length > extensionLength && fileName[fileName.Length - (extensionLength + 1)] == '.')
-                return fileName.Substring(0, fileName.Length - (extensionLength + 1));
-            else
-                return fileName;
-        }
-
-        static string GetExtension(string fileName)
-        {
-            try
+            int i = fileName.LastIndexOf('.');
+            if (i != -1)
             {
-                if (fileName == null)
+                bool hasDotSlash = false;
+
+                if (i == fileName.Length - 1)
                 {
                     return "";
                 }
 
-
-                int i = fileName.LastIndexOf('.');
-                if (i != -1)
+                if (i < fileName.Length + 1 && (fileName[i + 1] == '/' || fileName[i + 1] == '\\'))
                 {
-                    bool hasDotSlash = false;
+                    hasDotSlash = true;
+                }
 
-                    if (i == fileName.Length - 1)
-                    {
-                        return "";
-                    }
-
-                    if (i < fileName.Length + 1 && (fileName[i + 1] == '/' || fileName[i + 1] == '\\'))
-                    {
-                        hasDotSlash = true;
-                    }
-
-                    if (hasDotSlash)
-                    {
-                        return "";
-                    }
-                    else
-                    {
-                        return fileName.Substring(i + 1, fileName.Length - (i + 1)).ToLower();
-                    }
+                if (hasDotSlash)
+                {
+                    return "";
                 }
                 else
                 {
-                    return ""; // This returns "" because calling the method with a string like "redball" should return no extension
+                    return fileName.Substring(i + 1, fileName.Length - (i + 1)).ToLower();
                 }
             }
-            catch
+            else
             {
-                //EMP: Removed to clean up Warnings
-                //int m = 3;
-                throw new Exception();
+                return ""; // This returns "" because calling the method with a string like "redball" should return no extension
             }
+        }
+        catch
+        {
+            //EMP: Removed to clean up Warnings
+            //int m = 3;
+            throw new Exception();
         }
     }
 }
