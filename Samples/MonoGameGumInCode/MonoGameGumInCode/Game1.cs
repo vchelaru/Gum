509a30c35bd0c875821dd333eec8d5d831fb1776
Victor <VicChelaru@gmail.com>
2023-12-18 18:44:45 -0700

    Added SpriteRuntime

--- Samples/MonoGameGumInCode/MonoGameGumInCode/Game1.cs
+++ Samples/MonoGameGumInCode/MonoGameGumInCode/Game1.cs
@@ -24,28 +24,48 @@ namespace MonoGameGumInCode
             SystemManagers.Default = new SystemManagers();
             SystemManagers.Default.Initialize(_graphics.GraphicsDevice, fullInstantiation: true);
 
+            CreateLayout();
+
+            base.Initialize();
+        }
+
+        private static void CreateLayout()
+        {
             var container = new ContainerRuntime();
             container.WidthUnits = Gum.DataTypes.DimensionUnitType.RelativeToContainer;
             container.HeightUnits = Gum.DataTypes.DimensionUnitType.RelativeToContainer;
             container.Width = 0;
             container.Height = 0;
             container.ChildrenLayout = Gum.Managers.ChildrenLayout.TopToBottomStack;
-
             container.AddToManagers();
 
+            AddText(container, "This is a rectangle:");
 
-            var TextInstance = new TextRuntime();
-            TextInstance.WidthUnits = Gum.DataTypes.DimensionUnitType.RelativeToChildren;
-            TextInstance.Width = 0;
-            TextInstance.Text = "This is a rectangle:";
-            container.Children.Add(TextInstance);
-            //TextInstance.AddToManagers();
+            var coloredRectangleInstance = new ColoredRectangleRuntime();
+            coloredRectangleInstance.X = 10;
+            coloredRectangleInstance.Y = 10;
+            coloredRectangleInstance.Width = 120;
+            coloredRectangleInstance.Height = 48;
+            coloredRectangleInstance.Color = Color.White;
+            container.Children.Add(coloredRectangleInstance);
 
+            AddText(container, "This is a sprite:");
 
+            var sprite = new SpriteRuntime();
+            sprite.X = 10;
+            sprite.Y = 10;
+            sprite.SourceFileName = "Content/BearTexture.png";
+            container.Children.Add(sprite);
 
+        }
 
-
-            base.Initialize();
+        private static void AddText(ContainerRuntime container, string text)
+        {
+            var textInstance = new TextRuntime();
+            textInstance.WidthUnits = Gum.DataTypes.DimensionUnitType.RelativeToChildren;
+            textInstance.Width = 0;
+            textInstance.Text = text;
+            container.Children.Add(textInstance);
         }
 
         protected override void LoadContent()
