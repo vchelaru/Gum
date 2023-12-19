fe155d0385a8f5da226bce70165de9103094abe1
Victor <VicChelaru@gmail.com>
2023-12-18 19:32:34 -0700

    Started making new sample project

---
+++ Samples/MonoGameGumFromFile/MonoGameGumFromFile/Game1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using RenderingLibrary;
+
+namespace MonoGameGumFromFile
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            SystemManagers.Default = new SystemManagers();
+            SystemManagers.Default.Initialize(_graphics.GraphicsDevice, fullInstantiation: true);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            SystemManagers.Default.Activity(gameTime.TotalGameTime.TotalSeconds);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            SystemManagers.Default.Draw();
+
+            base.Draw(gameTime);
+        }
+    }
+}
