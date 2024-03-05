952592f65a9c2271ff38df2da4d306b566332904
Victor Chelaru <VicChelaru@gmail.com>
2024-03-05 07:00:07 -0700

    Added delete of all bin folders in PS1

--- DeleteBinScript.ps1
+++ DeleteBinScript.ps1
@@ -1,1 +1,4 @@
-Remove-Item -Path "ToolsUtilities/bin" -Recurse -Force
+Remove-Item -Path "GumCommon/bin" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "GumDataTypes/bin" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "MonoGameGum/bin" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "ToolsUtilities/bin" -Recurse -Force -ErrorAction SilentlyContinue
