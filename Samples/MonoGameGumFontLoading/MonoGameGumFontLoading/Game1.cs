0c6e7e752830608dc74bd23ce1dba30fca605391
Victor <VicChelaru@gmail.com>
2024-04-25 19:15:16 -0600

    Added sample fnt loading project

---
+++ Samples/MonoGameGumFontLoading/MonoGameGumFontLoading/Game1.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGameGum.GueDeriving;
+using RenderingLibrary;
+
+namespace MonoGameGumFontLoading
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            SystemManagers.Default = new SystemManagers();
+            SystemManagers.Default.Initialize(_graphics.GraphicsDevice, fullInstantiation: true);
+
+
+            var text = new TextRuntime();
+            text.UseCustomFont = true;
+            text.CustomFontFile = "Fonts/Font16Jing_Jing.fnt";
+            text.Text = "I use Jing_Jing";
+            text.AddToManagers(SystemManagers.Default, null);
+
+            var text2 = new TextRuntime();
+            text2.UseCustomFont = true;
+            text2.CustomFontFile = "Fonts/Font18Arial.fnt";
+            text2.Text = "I use Arial 18";
+            text2.AddToManagers(SystemManagers.Default, null);
+            text2.Y = 30;
+
+            var text3 = new TextRuntime();
+            text3.UseCustomFont = true;
+            text3.CustomFontFile = "Fonts/Font18Bahnschrift_Light.fnt";
+            text3.Text = "I use Font18Bahnschrift Light";
+            text3.AddToManagers(SystemManagers.Default, null);
+            text3.Y = 60;
+
+            base.Initialize();
+        }
+
+
+        protected override void Update(GameTime gameTime)
+        {
+            SystemManagers.Default.Activity(gameTime.TotalGameTime.TotalSeconds);
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            SystemManagers.Default.Draw();
+            base.Draw(gameTime);
+        }
+    }
+}
