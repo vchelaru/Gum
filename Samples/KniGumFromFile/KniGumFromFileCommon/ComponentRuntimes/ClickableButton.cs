ae6b7b834e9700863c16eba8a5b349365514ab4a
Nikos Kastellanos <nkastellanos@gmail.com>
2024-06-26 01:44:26 +0300

    KniGumFromFile

---
+++ Samples/KniGumFromFile/KniGumFromFileCommon/ComponentRuntimes/ClickableButton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gum.Wireframe;
+
+namespace KniGumFromFile.ComponentRuntimes;
+
+internal class ClickableButton : InteractiveGue
+{
+    GraphicalUiElement textInstance;
+
+    public ClickableButton() : base() 
+    {
+        this.Click += HandleClick;
+        this.RollOn += (_, _) => System.Diagnostics.Debug.WriteLine($"Roll On {Name}");
+        this.RollOff += (_, _) => System.Diagnostics.Debug.WriteLine($"Roll Off @ {Name}");
+    }
+
+    int NumberOfClicks;
+
+    public override void AfterFullCreation()
+    {
+        textInstance = GetGraphicalUiElementByName("TextInstance");
+    }
+
+    public void HandleClick(object sender, EventArgs args)
+    {
+
+        NumberOfClicks++;
+
+        textInstance.SetProperty("Text", "Clicked " + NumberOfClicks + " times");
+    }
+}
