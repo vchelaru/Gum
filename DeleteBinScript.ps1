2e2e4465cc4c4fe07a11fe460f12eff63616df4d
Victor Chelaru <VicChelaru@gmail.com>
2024-07-22 05:25:03 -0600

    Updated build script to delete kni gum.

--- DeleteBinScript.ps1
+++ DeleteBinScript.ps1
@@ -1,4 +1,5 @@
 Remove-Item -Path "GumCommon/bin" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "GumDataTypes/bin" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "MonoGameGum/bin" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "MonoGameGum/KniGum/bin" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "ToolsUtilities/bin" -Recurse -Force -ErrorAction SilentlyContinue
