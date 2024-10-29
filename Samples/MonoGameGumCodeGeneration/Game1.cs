c5f5231aff18787d35fa9eddd95683ad03307e5a
Victor <VicChelaru@gmail.com>
2024-10-29 05:11:22 -0600

    Added initial project

---
+++ Samples/MonoGameGumCodeGeneration/Game1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using RenderingLibrary;
+
+namespace MonoGameGumCodeGeneration
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            SystemManagers.Default = new SystemManagers(); 
+            SystemManagers.Default.Initialize(_graphics.GraphicsDevice, fullInstantiation: true);
+            base.Initialize();
+        }
+
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            SystemManagers.Default.Activity(gameTime.TotalGameTime.TotalSeconds);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            SystemManagers.Default.Draw();
+
+            base.Draw(gameTime);
+        }
+    }
+}
