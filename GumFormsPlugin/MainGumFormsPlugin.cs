e5bfba9246a60a50f1f7460bf4a072c6513d727e
Victor <VicChelaru@gmail.com>
2024-11-17 13:31:39 -0700

    More fixes on the demo screen
    Adding Gum Forms now can include an optional demo screen.

--- GumFormsPlugin/MainGumFormsPlugin.cs
+++ GumFormsPlugin/MainGumFormsPlugin.cs
@@ -4,6 +4,7 @@ using Gum.Plugins;
 using Gum.Plugins.BaseClasses;
 using Gum.Plugins.ImportPlugin.Manager;
 using Gum.ToolStates;
+using GumFormsPlugin.Views;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
@@ -19,16 +20,8 @@ namespace GumFormsPlugin;
 internal class MainGumFormsPlugin : PluginBase
 {
     public override string FriendlyName => "Gum Forms Plugin";
-
-    // for now, we make this false, we can add screens later:
-    bool saveScreens = false;
-
     public override Version Version => new Version(1, 0);
-
-    public override bool ShutDown(PluginShutDownReason shutDownReason)
-    {
-        return true;
-    }
+    public override bool ShutDown(PluginShutDownReason shutDownReason) => true;
 
     public override void StartUp()
     {
@@ -46,171 +39,11 @@ internal class MainGumFormsPlugin : PluginBase
         }
         #endregion
 
-        var sourceDestinations = GetSourceDestinations();
-        bool canSaveFiles = GetIfShouldSave(sourceDestinations);
-
-        if (canSaveFiles)
-        {
-            SaveFilesToDestination(sourceDestinations);
-
-            // now add all components and screens to the project
-            AddAllElementsToProject(sourceDestinations);
-
-            // reload standards:
-            var fileName = GumState.Self.ProjectState.GumProjectSave.FullFileName;
-            bool wasSaved = GumCommands.Self.FileCommands.TryAutoSaveProject();
-            if (wasSaved)
-            {
-                GumCommands.Self.FileCommands.LoadProject(fileName);
-            }
-            else
-            {
-                GumCommands.Self.GuiCommands.ShowMessage("You must Save, then close/reopen the project.");
-            }
-        }
-    }
-
-    private void AddAllElementsToProject(Dictionary<string, FilePath> sourceDestinations)
-    {
-        foreach(var item in sourceDestinations)
-        {
-            var extension = item.Value.Extension;
-
-            if (extension == "gusx")
-            {
-                // add screen
-                ImportLogic.ImportScreen(item.Value, saveProject:false);
-            }
-            else if(extension == "gucx")
-            {
-                // add component
-                ImportLogic.ImportComponent(item.Value, saveProject: false);
-            }
-            else if(extension == "behx")
-            {
-                // add behavior
-                ImportLogic.ImportBehavior(item.Value, saveProject: false);
-            }
-            // standards are already added
-        }
+        var view = new AddFormsWindow();
+        view.ShowDialog();
 
     }
 
-    private void SaveFilesToDestination(Dictionary<string, FilePath> sourceDestinations)
-    {
-        var assembly = GetType().Assembly;
-        foreach (var kvp in sourceDestinations)
-        {
-            var source = kvp.Key;
-            var destination = kvp.Value;
-
-            var extension = destination.Extension;
-
-            // don't save the project, we don't want to overwrite it because that would wipe existing
-            // projects which may have screens or other components referenced.
-
-            var isGumx = extension == "gumx";
-
-            var shouldSave = isGumx == false && extension != "gusx";
-
-            if (shouldSave)
-            {
-                FileManager.SaveEmbeddedResource(assembly, kvp.Key, kvp.Value.FullPath);
-            }
-        }
-    }
-
-    private Dictionary<string, FilePath> GetSourceDestinations()
-    {
-        var assembly = GetType().Assembly;
-
-        var resourceNames = assembly.GetManifestResourceNames();
-
-        var destinationFolder = GumState.Self.ProjectState.ProjectDirectory;
-
-        const string resourcePrefix =
-            "GumFormsPlugin.Content.FormsGumProject.";
-
-        var resourcesToSave = resourceNames.Where(item =>
-            item.StartsWith(resourcePrefix));
-
-        Dictionary<string, FilePath> sourceDestinations = new Dictionary<string, FilePath>();
-
-        foreach (var resourceName in resourcesToSave)
-        {
-            var extension = FileManager.GetExtension(resourceName);
-
-            if(extension == "gusx" && saveScreens == false)
-            {
-                continue;
-            }
-            if(extension == "gumx")
-            {
-                continue;
-            }
-
-            var stripped = resourceName.Substring(resourcePrefix.Length);
-
-            var name = FileManager.RemoveExtension(stripped).Replace(".", "/")
-                + "." + extension;
-
-            var absoluteDestination = destinationFolder + name;
-
-            sourceDestinations.Add(resourceName, absoluteDestination);
-        }
-
-        return sourceDestinations;
-    }
-    private static bool GetIfShouldSave(Dictionary<string, FilePath> sourceDestinations)
-    {
-        var existingFiles = sourceDestinations.Values.Where(item => item.Exists());
-
-        var doStandardsExist = existingFiles.Any(item => item.Extension == "gutx");
-        var nonStandardFiles = existingFiles.Where(item => item.Extension != "gutx").ToList();
-
-        var nonStandardWhichBlockCopying = nonStandardFiles.Where(item =>
-        {
-            // don't block on gumx:
-            if(item.Extension == "gumx")
-            {
-                return false;
-            }
-            return true;
-        }).ToList();
-
-
-        var shouldSave = false;
-        if (nonStandardWhichBlockCopying.Count > 0)
-        {
-            var message = "Cannot create Gum project because the following file(s) would get overwritten:"
-                + "\n\n" + string.Join("\n", nonStandardFiles);
-            GumCommands.Self.GuiCommands.ShowMessage(message);
-        }
-        else if (doStandardsExist)
-        {
-            var filesWhichWouldGetOverwritten = existingFiles
-                .Where(item =>
-                {
-                    return item.Extension != "gumx";
-                })
-                .Select(item => item.RelativeTo(GumState.Self.ProjectState.ProjectDirectory))
-                .ToList();
-
-
-
-            var message = "Creating Forms objects would result in overwriting the following files"
-                + "\n\n" + string.Join("\n", filesWhichWouldGetOverwritten) + "\nProceed?";
-
-            shouldSave = GumCommands.Self.GuiCommands.ShowYesNoMessageBox(message) == System.Windows.MessageBoxResult.Yes;
-        }
-        else
-        {
-            // I guess the user completely deleted everything?
-            shouldSave = true;
-        }
-
-        return shouldSave;
-    }
 }
 
 
---
+++ GumFormsPlugin/Views/AddFormsWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using GumFormsPlugin.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GumFormsPlugin.Views
+{
+    /// <summary>
+    /// Interaction logic for AddFormsWindow.xaml
+    /// </summary>
+    public partial class AddFormsWindow : Window
+    {
+        AddFormsViewModel ViewModel => DataContext as AddFormsViewModel;
+        public AddFormsWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new ViewModels.AddFormsViewModel();
+        }
+
+        private void OkButtonClicked(object sender, RoutedEventArgs e)
+        {
+            ViewModel.DoIt();
+            this.DialogResult = true;
+
+        }
+
+        private void CancelButtonClicked(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
