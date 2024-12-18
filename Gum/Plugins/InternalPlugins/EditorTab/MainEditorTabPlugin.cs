893045ebf69c9785476f9fc596609a55dc97d1a8
Victor <VicChelaru@gmail.com>
2024-12-18 16:46:30 -0700

    Fixes editor visuals not reflecting selected state

---
+++ Gum/Plugins/InternalPlugins/EditorTab/MainEditorTabPlugin.cs
@@ -0,0 +1,30 @@
+﻿using Gum.DataTypes.Variables;
+using Gum.Plugins.BaseClasses;
+using Gum.Wireframe;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gum.Plugins.InternalPlugins.EditorTab;
+
+[Export(typeof(PluginBase))]
+internal class MainEditorTabPlugin : InternalPlugin
+{
+    public override void StartUp()
+    {
+        AssignEvents();
+    }
+
+    private void AssignEvents()
+    {
+        this.ReactToStateSaveSelected += HandleStateSelected;
+    }
+
+    private void HandleStateSelected(StateSave save)
+    {
+        WireframeObjectManager.Self.RefreshAll(forceLayout: true);
+    }
+}
