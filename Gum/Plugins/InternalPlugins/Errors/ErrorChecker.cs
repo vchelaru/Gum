e73d078156ff8337fb7f6c7dd6cf07439f8897dd
Victor Chelaru <VicChelaru@gmail.com>
2025-01-23 19:59:05 -0700

    Missing required variables from behaviors are now reported as errors on components

--- Gum/Plugins/InternalPlugins/Errors/ErrorChecker.cs
+++ Gum/Plugins/InternalPlugins/Errors/ErrorChecker.cs
@@ -1,7 +1,12 @@
 ﻿using Gum.DataTypes;
+using Gum.DataTypes.Behaviors;
+using Gum.DataTypes.Variables;
 using Gum.Managers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Forms;
+using Xceed.Wpf.AvalonDock.Themes;
 
 namespace Gum.Plugins.Errors
 {
@@ -48,63 +53,105 @@ namespace Gum.Plugins.Errors
             return toReturn;
         }
 
-        private static void AddBehaviorErrors(ComponentSave component, List<ErrorViewModel> toReturn, DataTypes.Behaviors.BehaviorSave behavior)
+        private static void AddBehaviorErrors(ComponentSave component, List<ErrorViewModel> errorList, DataTypes.Behaviors.BehaviorSave behavior)
         {
             foreach (var behaviorInstance in behavior.RequiredInstances)
             {
-                var candidateInstances = component.Instances.Where(item => item.Name == behaviorInstance.Name).ToList();
+                AddErrorsForBehaviorInstance(component, errorList, behavior, behaviorInstance);
+            }
+
+            // January 23, 2025
+            // Vic says:
+            // Not sure if we even support veraible lists in behaviors at this point, so
+            // let's just worry about behaviors
+            foreach(var behaviorVariable in behavior.RequiredVariables.Variables)
+            {
+                AddErrorsForBehaviorVariable(component, errorList, behavior, behaviorVariable);
+            }
+        }
+
+        private static void AddErrorsForBehaviorVariable(ComponentSave component, List<ErrorViewModel> toReturn, BehaviorSave behavior, VariableSave behaviorVariable)
+        {
+            var rfv = new RecursiveVariableFinder(component.DefaultState);
+            var variable = rfv.GetVariable(behaviorVariable.Name);
+
+            if(variable == null)
+            {
+
+                toReturn.Add(new ErrorViewModel
+                {
+                    Message = $"The behavior {behavior} " +
+                            $"requires a variable named {behaviorVariable.Name} but this variable doesn't exist. " +
+                            $"Add a custom variable or expose a variable and give it the required name to solve this error."
+                });
+            }
+            else if(variable.Type != behaviorVariable.Type)
+            {
+                toReturn.Add(new ErrorViewModel
+                {
+                    Message = $"The behavior {behavior} " +
+                            $"requires a variable named {behaviorVariable.Name} with type {behaviorVariable.Type}. " +
+                            $"This variable exists but it has the wrong type {variable.Type};"
+                });
+            }
+
+            // no errors
+        }
+
+        private static void AddErrorsForBehaviorInstance(ComponentSave component, List<ErrorViewModel> toReturn, BehaviorSave behavior, BehaviorInstanceSave behaviorInstance)
+        {
+            var candidateInstances = component.Instances.Where(item => item.Name == behaviorInstance.Name).ToList();
+            if (!string.IsNullOrEmpty(behaviorInstance.BaseType))
+            {
+                candidateInstances = candidateInstances.Where(item => item.IsOfType(behaviorInstance.BaseType)).ToList();
+            }
+
+            if (behaviorInstance.Behaviors.Any())
+            {
+                var requiredBehaviorNames = behaviorInstance.Behaviors.Select(item => item.Name);
+                candidateInstances = candidateInstances.Where(item =>
+                {
+                    bool fulfillsRequirements = false;
+                    var element = ObjectFinder.Self.GetComponent(item.BaseType);
+                    if (element != null)
+                    {
+                        var implementedBehaviorNames = element.Behaviors.Select(implementedBehavior => implementedBehavior.BehaviorName);
+
+                        fulfillsRequirements = requiredBehaviorNames.All(required => implementedBehaviorNames.Contains(required));
+
+                    }
+                    return fulfillsRequirements;
+                }).ToList();
+
+            }
+
+            if (!candidateInstances.Any())
+            {
+                string message = $"Missing instance with name {behaviorInstance.Name}";
                 if (!string.IsNullOrEmpty(behaviorInstance.BaseType))
                 {
-                    candidateInstances = candidateInstances.Where(item => item.IsOfType(behaviorInstance.BaseType)).ToList();
+                    message += $" of type {behaviorInstance.BaseType}";
                 }
-
                 if (behaviorInstance.Behaviors.Any())
                 {
-                    var requiredBehaviorNames = behaviorInstance.Behaviors.Select(item => item.Name);
-                    candidateInstances = candidateInstances.Where(item =>
+                    if (behaviorInstance.Behaviors.Count == 1)
                     {
-                        bool fulfillsRequirements = false;
-                        var element = ObjectFinder.Self.GetComponent(item.BaseType);
-                        if (element != null)
-                        {
-                            var implementedBehaviorNames = element.Behaviors.Select(implementedBehavior => implementedBehavior.BehaviorName);
-
-                            fulfillsRequirements = requiredBehaviorNames.All(required => implementedBehaviorNames.Contains(required));
-
-                        }
-                        return fulfillsRequirements;
-                    }).ToList();
-
+                        message += " with behavior type ";
+                    }
+                    else
+                    {
+                        message += " with behavior types ";
+                    }
+                    var behaviorsJoined = string.Join(", ", behaviorInstance.Behaviors.Select(item => item.Name).ToArray());
+                    message += behaviorsJoined;
                 }
 
-                if (!candidateInstances.Any())
+                message += $" needed by behavior {behavior.Name}";
+
+                toReturn.Add(new ErrorViewModel
                 {
-                    string message = $"Missing instance with name {behaviorInstance.Name}";
-                    if (!string.IsNullOrEmpty(behaviorInstance.BaseType))
-                    {
-                        message += $" of type {behaviorInstance.BaseType}";
-                    }
-                    if(behaviorInstance.Behaviors.Any())
-                    {
-                        if(behaviorInstance.Behaviors.Count == 1)
-                        {
-                            message += " with behavior type ";
-                        }
-                        else
-                        {
-                            message += " with behavior types ";
-                        }
-                        var behaviorsJoined = string.Join(", ", behaviorInstance.Behaviors.Select(item => item.Name).ToArray());
-                        message += behaviorsJoined;
-                    }
-
-                    message += $" needed by behavior {behavior.Name}";
-
-                    toReturn.Add(new ErrorViewModel
-                    {
-                        Message = message
-                    });
-                }
+                    Message = message
+                });
             }
         }
     }
