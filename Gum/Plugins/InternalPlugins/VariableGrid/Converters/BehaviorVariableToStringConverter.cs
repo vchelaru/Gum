70c7cfc1e5125fec8cb4ab03bfcd114607a9f03c
Victor Chelaru <VicChelaru@gmail.com>
2025-01-23 20:46:23 -0700

    Removed variable value display in behaviors.

---
+++ Gum/Plugins/InternalPlugins/VariableGrid/Converters/BehaviorVariableToStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using Gum.DataTypes.Variables;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Xml.Linq;
+
+namespace Gum.Plugins.InternalPlugins.VariableGrid.Converters;
+public class BehaviorVariableToStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if(value is VariableSave variableSave)
+        {
+            return variableSave.Name + " (" + variableSave.Type + ")";
+        }
+        return string.Empty;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        // do nothing...
+        return null;
+    }
+}
