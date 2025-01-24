b5a2ff48d7bea26c39f896227fbd2eaed71621b6
Victor Chelaru <VicChelaru@gmail.com>
2025-01-23 17:54:02 -0700

    Started edit/delete variable right click menu items

--- Gum/Plugins/InternalPlugins/VariableGrid/MainControlViewModel.cs
+++ Gum/Plugins/InternalPlugins/VariableGrid/MainControlViewModel.cs
@@ -1,7 +1,11 @@
 ﻿using Gum.DataTypes.Variables;
 using Gum.Mvvm;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Media;
 
 namespace Gum.Plugins.VariableGrid
@@ -38,11 +42,15 @@ namespace Gum.Plugins.VariableGrid
             set => Set(value); 
         }
 
+        #region Behaviors
+
         public Visibility ShowBehaviorUi
         {
             get { return Get<Visibility>(); }
             set { Set(value); }
         }
+
+
 
         public ObservableCollection<VariableSave> BehaviorVariables
         {
@@ -50,19 +58,42 @@ namespace Gum.Plugins.VariableGrid
             private set;
         } = new ObservableCollection<VariableSave>();
 
-        public Visibility ShowVariableGrid
-        {
-            //get => Visibility.Hidden;
-            get => Get<Visibility>();
-            set => Set(value);
-        }
-
         public VariableSave SelectedBehaviorVariable
         {
             get => Get<VariableSave>(); 
             set => Set(value); 
         }
 
+        public List<MenuItem> BehaviorVariablesContextMenuItems
+        {
+            get
+            {
+                if(SelectedBehaviorVariable == null)
+                {
+                    return new List<MenuItem>();
+                }
+                else
+                {
+                    return new List<MenuItem>()
+                    {
+                        EditVariableMenuItem,
+                        DeleteVariableMenuItem
+                    };
+                }
+            }
+        }
+
+        MenuItem EditVariableMenuItem;
+        MenuItem DeleteVariableMenuItem;
+
+        #endregion
+
+        public Visibility ShowVariableGrid
+        {
+            //get => Visibility.Hidden;
+            get => Get<Visibility>();
+            set => Set(value);
+        }
         public Visibility AddVariableButtonVisibility
         {
             get => Get<Visibility>();
@@ -82,6 +113,27 @@ namespace Gum.Plugins.VariableGrid
                     return null;
                 }
             }
+        }
+
+        public MainControlViewModel()
+        {
+            EditVariableMenuItem = new MenuItem();
+            EditVariableMenuItem.Header = "Edit Variable";
+            EditVariableMenuItem.Click += HandleEditVariableClicked;
+
+            DeleteVariableMenuItem = new MenuItem();
+            DeleteVariableMenuItem.Header = "Delete Variable";
+            DeleteVariableMenuItem.Click += handleDeleteVariableClicked;
+        }
+
+        private void handleDeleteVariableClicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HandleEditVariableClicked(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
