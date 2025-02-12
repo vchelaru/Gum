38e6aa9a14714233a7fef4ab51568361eb90328a
Victor Chelaru <VicChelaru@gmail.com>
2025-02-11 18:05:14 -0700

    Added StackPanel

---
+++ MonoGameGum/Forms/Controls/Image.cs
@@ -0,0 +1,27 @@
+﻿using RenderingLibrary.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#if FRB
+using InteractiveGue = global::Gum.Wireframe.GraphicalUiElement;
+namespace FlatRedBall.Forms.Controls;
+#else
+namespace MonoGameGum.Forms.Controls;
+#endif
+
+public class Image : FrameworkElement
+{
+    public Image() :
+        base(new Gum.Wireframe.InteractiveGue(new Sprite(texture:null)))
+    {
+        Visual.Width = 100;
+        Visual.WidthUnits = Gum.DataTypes.DimensionUnitType.PercentageOfSourceFile;
+        Visual.Height = 100;
+        Visual.HeightUnits = Gum.DataTypes.DimensionUnitType.PercentageOfSourceFile;
+
+        IsVisible = true;
+    }
+}
---
+++ MonoGameGum/Forms/Controls/StackPanel.cs
@@ -0,0 +1,71 @@
+﻿using Gum.Wireframe;
+using RenderingLibrary.Graphics;
+
+#if FRB
+using InteractiveGue = global::Gum.Wireframe.GraphicalUiElement;
+namespace FlatRedBall.Forms.Controls;
+#else
+namespace MonoGameGum.Forms.Controls;
+
+public enum Orientation
+{
+    Horizontal = 0,
+    Vertical = 1
+}
+
+#endif
+
+public class StackPanel : FrameworkElement
+{
+    Orientation orientation = Orientation.Vertical;
+    public Orientation Orientation
+    {
+        get { return orientation; }
+        set
+        {
+            if(value != orientation)
+            {
+                orientation = value;
+                UpdateToOrientation();
+            }
+        }
+    }
+
+    protected override void ReactToVisualChanged()
+    {
+        base.ReactToVisualChanged();
+        Visual.ExposeChildrenEvents = true;
+        
+        UpdateToOrientation();
+    }
+
+    public StackPanel() : 
+        base(new InteractiveGue(new InvisibleRenderable()))
+        //base(new global::Gum.Wireframe.GraphicalUiElement(new InvisibleRenderable(), null))
+    {
+        Width = 0;
+        Visual.WidthUnits = global::Gum.DataTypes.DimensionUnitType.RelativeToContainer;
+        Height = 10;
+        Visual.HeightUnits = global::Gum.DataTypes.DimensionUnitType.RelativeToChildren;
+
+        IsVisible = true;
+
+    }
+
+    private void UpdateToOrientation()
+    {
+        if(Visual != null)
+        {
+            if(Orientation == Orientation.Horizontal)
+            {
+                Visual.ChildrenLayout = 
+                    global::Gum.Managers.ChildrenLayout.LeftToRightStack;
+            }
+            else
+            {
+                Visual.ChildrenLayout =
+                    global::Gum.Managers.ChildrenLayout.TopToBottomStack;
+            }
+        }
+    }
+}
