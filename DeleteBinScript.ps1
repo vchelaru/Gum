f1d0c2905aabbf68f995c72b40910223ac43e269
Victor Chelaru <VicChelaru@gmail.com>
2025-02-26 21:40:17 -0700

    Added deletion of FNA bin folder
    Upped nuget for testing fna

--- DeleteBinScript.ps1
+++ DeleteBinScript.ps1
@@ -1,6 +1,7 @@
 Remove-Item -Path "GumCommon/bin" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "GumDataTypes/bin" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "MonoGameGum/bin" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "MonoGameGum/FnaGum/bin" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "MonoGameGum/KniGum/bin" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "ToolsUtilities/bin" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "SkiaGum/bin" -Recurse -Force -ErrorAction SilentlyContinue
