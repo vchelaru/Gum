fdc846aadcd8ff92ef7692573db731030f9bb1ad
Victor Chelaru <VicChelaru@gmail.com>
2025-04-22 06:39:39 -0600

    Created GumStateAnimation library and Gum tool references it.

---
+++ Libraries/GumStateAnimation/GumStateAnimation/SaveClasses/NamedEventSave.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateAnimationPlugin.SaveClasses
+{
+    public class NamedEventSave
+    {
+        public string Name { get; set; }
+        public float Time { get; set; }
+
+    }
+}
