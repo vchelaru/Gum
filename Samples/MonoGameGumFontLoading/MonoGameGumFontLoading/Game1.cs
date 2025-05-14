cdcf41d31a18296d15012bd30195f574dcc1a616
tonybierman <tonybierman@gmail.com>
2025-05-14 05:17:33 -0500

    A few housekeeping tasks in MonoGame samples, e.g. implementing Disose

--- Samples/MonoGameGumFontLoading/MonoGameGumFontLoading/Game1.cs
+++ Samples/MonoGameGumFontLoading/MonoGameGumFontLoading/Game1.cs
@@ -1,10 +1,10 @@
 ﻿using Gum.Wireframe;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 using MonoGameGum.GueDeriving;
 using RenderingLibrary;
-using RenderingLibrary.Graphics;
+using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace MonoGameGumFontLoading
@@ -12,6 +12,8 @@ namespace MonoGameGumFontLoading
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
+        private readonly List<TextRuntime> _texts = new List<TextRuntime>();
+        private bool _disposed;
 
         public Game1()
         {
@@ -25,51 +27,45 @@ namespace MonoGameGumFontLoading
             SystemManagers.Default = new SystemManagers();
             SystemManagers.Default.Initialize(_graphics.GraphicsDevice, fullInstantiation: true);
 
-            // fonts can be explicitly loaded too:
-            var bitmapFont = new BitmapFont("Fonts/Font18Arial.fnt", SystemManagers.Default);
+            AddText("Fonts/Font16Jing_Jing.fnt", "I use Jing_Jing", 0);
+            AddText("Fonts/Font18Arial.fnt", "I use Arial 18", 30);
+            AddText("Fonts/Font18Bahnschrift_Light.fnt", "I use Font18Bahnschrift Light", 60);
 
-            var text = new TextRuntime();
-            text.UseCustomFont = true;
-            text.CustomFontFile = "Fonts/Font16Jing_Jing.fnt";
-            text.Text = "I use Jing_Jing";
-            text.AddToManagers(SystemManagers.Default, null);
-            // No errors here:
-            GraphicalUiElement.ThrowExceptionsForMissingFiles(text);
-
-            var text2 = new TextRuntime();
-            text2.UseCustomFont = true;
-            text2.CustomFontFile = "Fonts/Font18Arial.fnt";
-            text2.Text = "I use Arial 18";
-            text2.AddToManagers(SystemManagers.Default, null);
-            text2.Y = 30;
-            // No errors here:
-            GraphicalUiElement.ThrowExceptionsForMissingFiles(text2);
-
-            var text3 = new TextRuntime();
-            text3.UseCustomFont = true;
-            text3.CustomFontFile = "Fonts/Font18Bahnschrift_Light.fnt";
-            text3.Text = "I use Font18Bahnschrift Light";
-            text3.AddToManagers(SystemManagers.Default, null);
-            text3.Y = 60;
-            // No errors here:
-            GraphicalUiElement.ThrowExceptionsForMissingFiles(text3);
-
-
-            try
-            {
-                var textThatHasError = new TextRuntime();
-                textThatHasError.UseCustomFont = true;
-                textThatHasError.CustomFontFile = "Fonts/InvalidFont.fnt";
-                GraphicalUiElement.ThrowExceptionsForMissingFiles(textThatHasError);
-            }
-            catch (FileNotFoundException e)
-            {
-                System.Diagnostics.Debug.WriteLine("Yay we got an exception! That's expected");     
-            }
+            TryAddInvalidText("Fonts/InvalidFont.fnt");
 
             base.Initialize();
         }
 
+        private void AddText(string fontFile, string text, float yPosition)
+        {
+            var textRuntime = new TextRuntime
+            {
+                UseCustomFont = true,
+                CustomFontFile = fontFile,
+                Text = text,
+                Y = yPosition
+            };
+            textRuntime.AddToManagers(SystemManagers.Default, null);
+            GraphicalUiElement.ThrowExceptionsForMissingFiles(textRuntime);
+            _texts.Add(textRuntime);
+        }
+
+        private void TryAddInvalidText(string fontFile)
+        {
+            try
+            {
+                var textRuntime = new TextRuntime
+                {
+                    UseCustomFont = true,
+                    CustomFontFile = fontFile
+                };
+                GraphicalUiElement.ThrowExceptionsForMissingFiles(textRuntime);
+            }
+            catch (FileNotFoundException)
+            {
+                System.Diagnostics.Debug.WriteLine("Expected exception for invalid font.");
+            }
+        }
 
         protected override void Update(GameTime gameTime)
         {
@@ -82,24 +78,43 @@ namespace MonoGameGumFontLoading
             GraphicsDevice.Clear(Color.CornflowerBlue);
             SystemManagers.Default.Draw();
 
-
             var renderer = SystemManagers.Default.Renderer;
-
             renderer.Begin();
 
-            var text = new TextRuntime();
-            text.UseCustomFont = true;
-            text.CustomFontFile = "Fonts/Font16Jing_Jing.fnt";
-            text.Text = "I am an immediate mode TextRuntime";
-            text.X = 110;
-            text.Y = 120;
-
-            renderer.Draw(text);
+            var immediateText = new TextRuntime
+            {
+                UseCustomFont = true,
+                CustomFontFile = "Fonts/Font16Jing_Jing.fnt",
+                Text = "I am an immediate mode TextRuntime",
+                X = 110,
+                Y = 120
+            };
+            renderer.Draw(immediateText);
 
             renderer.End();
-
-
             base.Draw(gameTime);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                // Dispose managed resources
+                foreach (var text in _texts)
+                {
+                    text.RemoveFromManagers();
+                }
+                _texts.Clear();
+
+                _graphics?.Dispose();
+            }
+
+            _disposed = true;
+
+            base.Dispose(disposing);
         }
     }
 }
