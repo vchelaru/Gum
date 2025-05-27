69b251827c1a74b7751d7c610713540c1eb5974e
Victor <VicChelaru@gmail.com>
2025-05-27 11:27:35 -0600

    Fixed crash when drag+dropping a .png used by an existing .fnt file into the editor
    fixes #753

--- SkiaGum/Content/LoaderManager.cs
+++ SkiaGum/Content/LoaderManager.cs
@@ -2,135 +2,141 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace RenderingLibrary.Content
+namespace RenderingLibrary.Content;
+
+public class LoaderManager
 {
-    public class LoaderManager
+    #region Fields
+
+    static LoaderManager mSelf;
+
+
+    #endregion
+
+    #region Properties
+
+    public static LoaderManager Self
     {
-        #region Fields
-
-        static LoaderManager mSelf;
-
-
-        #endregion
-
-        #region Properties
-
-        public static LoaderManager Self
+        get
         {
-            get
+            if (mSelf == null)
             {
-                if (mSelf == null)
+                mSelf = new LoaderManager();
+            }
+            return mSelf;
+        }
+    }
+
+    public IContentLoader ContentLoader
+    {
+        get;
+        set;
+    }
+
+    // February 6, 2024
+    // Why is this false?
+    // Caching should be turned
+    // on by default...
+    //bool mCacheTextures = false;
+    bool mCacheTextures = true;
+    Dictionary<string, IDisposable> mCachedDisposables = new Dictionary<string, IDisposable>(StringComparer.OrdinalIgnoreCase);
+    public bool CacheTextures
+    {
+        get { return mCacheTextures; }
+        set
+        {
+            mCacheTextures = value;
+
+            if (!mCacheTextures)
+            {
+                foreach (KeyValuePair<string, IDisposable> kvp in mCachedDisposables)
                 {
-                    mSelf = new LoaderManager();
+                    kvp.Value.Dispose();
                 }
-                return mSelf;
+
+                mCachedDisposables.Clear();
+
             }
         }
+    }
 
-        public IContentLoader ContentLoader
-        {
-            get;
-            set;
-        }
-
-        // February 6, 2024
-        // Why is this false?
-        // Caching should be turned
-        // on by default...
-        //bool mCacheTextures = false;
-        bool mCacheTextures = true;
-        Dictionary<string, IDisposable> mCachedDisposables = new Dictionary<string, IDisposable>(StringComparer.OrdinalIgnoreCase);
-        public bool CacheTextures
-        {
-            get { return mCacheTextures; }
-            set
-            {
-                mCacheTextures = value;
-
-                if (!mCacheTextures)
-                {
-                    foreach (KeyValuePair<string, IDisposable> kvp in mCachedDisposables)
-                    {
-                        kvp.Value.Dispose();
-                    }
-
-                    mCachedDisposables.Clear();
-
-                }
-            }
-        }
-
-        #endregion
+    #endregion
 
 
-        public T LoadContent<T>(string contentName)
-        {
+    public T LoadContent<T>(string contentName)
+    {
 #if DEBUG
-            if (this.ContentLoader == null)
-            {
-                throw new Exception("The content loader is null - you must set it prior to calling LoadContent.");
-            }
+        if (this.ContentLoader == null)
+        {
+            throw new Exception("The content loader is null - you must set it prior to calling LoadContent.");
+        }
 #endif
 
-            return ContentLoader.LoadContent<T>(contentName);
-        }
+        return ContentLoader.LoadContent<T>(contentName);
+    }
 
-        public T TryLoadContent<T>(string contentName)
-        {
+    public T TryLoadContent<T>(string contentName)
+    {
 
 #if DEBUG
-            if (this.ContentLoader == null)
-            {
-                throw new Exception("The content loader is null - you must set it prior to calling LoadContent.");
-            }
+        if (this.ContentLoader == null)
+        {
+            throw new Exception("The content loader is null - you must set it prior to calling LoadContent.");
+        }
 #endif
-            return ContentLoader.TryLoadContent<T>(contentName);
+        return ContentLoader.TryLoadContent<T>(contentName);
+    }
+
+    public IDisposable GetDisposable(string name)
+    {
+        if (mCachedDisposables.ContainsKey(name))
+        {
+            return mCachedDisposables[name];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void AddDisposable(string name, IDisposable disposable)
+    {
+#if DEBUG
+        if(mCachedDisposables.ContainsKey(name))
+        {
+            throw new ArgumentException(
+                $"The cached disposable already contains an entry for {name}:{mCachedDisposables[name]}");
+        }
+#endif
+        mCachedDisposables.Add(name, disposable);
+    }
+
+    public void DisposeAndClear()
+    {
+        foreach (var item in mCachedDisposables.Values)
+        {
+            item.Dispose();
         }
 
-        public IDisposable GetDisposable(string name)
+        mCachedDisposables.Clear();
+    }
+
+    public void Dispose(string name)
+    {
+        if (mCachedDisposables.ContainsKey(name))
         {
-            if (mCachedDisposables.ContainsKey(name))
-            {
-                return mCachedDisposables[name];
-            }
-            else
-            {
-                return null;
-            }
+            mCachedDisposables[name].Dispose();
+            mCachedDisposables.Remove(name);
         }
+    }
 
-        public void AddDisposable(string name, IDisposable disposable)
+    public void RemoveWithoutDisposing(IDisposable disposable)
+    {
+        var kvp = mCachedDisposables.FirstOrDefault(item => item.Value == disposable);
+
+        if(kvp.Value == disposable)
         {
-            mCachedDisposables.Add(name, disposable);
-        }
-
-        public void DisposeAndClear()
-        {
-            foreach (var item in mCachedDisposables.Values)
-            {
-                item.Dispose();
-            }
-
-            mCachedDisposables.Clear();
-        }
-
-        public void Dispose(string name)
-        {
-            if (mCachedDisposables.ContainsKey(name))
-            {
-                mCachedDisposables[name].Dispose();
-                mCachedDisposables.Remove(name);
-            }
-        }
-
-        public void RemoveWithoutDisposing(IDisposable disposable)
-        {
-            var kvp = mCachedDisposables.FirstOrDefault(item => item.Value == disposable);
-
-            if(kvp.Value == disposable)
-            {
-                mCachedDisposables.Remove(kvp.Key);
-            }
+            mCachedDisposables.Remove(kvp.Key);
         }
     }
 }
