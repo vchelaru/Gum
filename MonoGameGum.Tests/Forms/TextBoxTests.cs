a88c0c894eb7a7c12268d8207e86bc1c011dc12d
Victor Chelaru <VicChelaru@gmail.com>
2025-06-03 18:39:34 -0600

    ENTER on TextBox and PasswordBox now apply value to binding if it exists...
    ...for TextBox this is only the case if AcceptsReturn is false.

---
+++ MonoGameGum.Tests/Forms/TextBoxTests.cs
@@ -0,0 +1,75 @@
+﻿using Gum.Mvvm;
+using MonoGameGum.Forms.Controls;
+using MonoGameGum.Forms.Data;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MonoGameGum.Tests.Forms;
+public class TextBoxTests
+{
+    [Fact]
+    public void HandleCharEntered_ShouldUpdateBindingSource_OnEnterNoAcceptsReturn()
+    {
+        TextBox textBox = new();
+        TestViewModel vm = new ()
+        {
+            Text = null
+        };
+        textBox.BindingContext = vm;
+        Binding binding = new (() => textBox.Text)
+        {
+            UpdateSourceTrigger = UpdateSourceTrigger.LostFocus
+        };
+
+        textBox.SetBinding(nameof(textBox.Text), binding);
+
+        textBox.AcceptsReturn = false;
+        textBox.Text = "Test text";
+        textBox.HandleCharEntered('\n'); // Simulate Enter key press
+
+        vm.Text.ShouldBe("Test text");
+    }
+
+    [Fact]
+    public void HandleCharEntered_ShouldNotUpdateBindingSource_OnEnterAcceptsReturn()
+    {
+
+        TextBox textBox = new();
+        TestViewModel vm = new ()
+        {
+            Text = null
+        };
+        textBox.BindingContext = vm;
+        Binding binding = new (() => textBox.Text)
+        {
+            UpdateSourceTrigger = UpdateSourceTrigger.LostFocus
+        };
+
+        textBox.SetBinding(nameof(textBox.Text), binding);
+
+        textBox.AcceptsReturn = false;
+        textBox.Text = "Test text";
+        textBox.HandleCharEntered('\n'); // Simulate Enter key press
+
+        vm.Text.ShouldBe(null);
+
+        textBox.IsFocused = false;
+
+        vm.Text.ShouldBe("Test text\n");
+    }
+
+
+    class TestViewModel : ViewModel
+    {
+        public string? Text
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+    }
+}
