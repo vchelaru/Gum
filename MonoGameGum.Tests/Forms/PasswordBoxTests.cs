21b00c29489db062f098a85914a96bb6199bd596
Victor Chelaru <VicChelaru@gmail.com>
2025-06-03 19:40:47 -0600

    ENTER on TextBox and PasswordBox now apply value to binding if it exi… (#827)
    * ENTER on TextBox and PasswordBox now apply value to binding if it exists...
    
    ...for TextBox this is only the case if AcceptsReturn is false.
    
    * Fixed newline TextBox unit test

---
+++ MonoGameGum.Tests/Forms/PasswordBoxTests.cs
@@ -0,0 +1,43 @@
+﻿using Gum.Mvvm;
+using MonoGameGum.Forms.Controls;
+using MonoGameGum.Forms.Data;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MonoGameGum.Tests.Forms;
+public class PasswordBoxTests
+{
+    [Fact]
+    public void HandleCharEntered_ShouldUpdateBindingSource_OnEnter()
+    {
+        PasswordBox passwordBox = new();
+        TestViewModel vm = new()
+        {
+            Password = null
+        };
+        passwordBox.BindingContext = vm;
+        Binding binding = new(() => passwordBox.Password)
+        {
+            UpdateSourceTrigger = UpdateSourceTrigger.LostFocus
+        };
+        passwordBox.SetBinding(nameof(passwordBox.Password), binding);
+        passwordBox.Password = "TestPassword";
+        passwordBox.HandleCharEntered('\n'); // Simulate Enter key press
+        vm.Password.ShouldBe("TestPassword");
+    }
+
+
+    class TestViewModel : ViewModel
+    {
+        public string? Password
+        {
+            get => Get<string?>();
+            set => Set(value);
+        }
+    }
+}
