0e4ce24311a7636840d8626cb7e82c94d505641c
Victor Chelaru <VicChelaru@gmail.com>
2025-06-05 07:28:05 -0600

    Code generation now properly generates Windows associations
    Default forms controls now sets WindowStandard as the default implementation for the Window behavior.

--- CodeOutputPlugin/Manager/CodeGenerator.cs
+++ CodeOutputPlugin/Manager/CodeGenerator.cs
@@ -1078,14 +1078,18 @@ public class CodeGenerator
     {
         formsType = null;
         behavior = null;
-        var behaviors = element.Behaviors;
-        foreach (var possibleBehavior in behaviors)
+        var behaviors = element?.Behaviors;
+
+        if(behaviors != null)
         {
-            if (BehaviorGumFormsTypes.ContainsKey(possibleBehavior.BehaviorName))
+            foreach (var possibleBehavior in behaviors)
             {
-                formsType = BehaviorGumFormsTypes[possibleBehavior.BehaviorName];
-                behavior = possibleBehavior;
-                break;
+                if (BehaviorGumFormsTypes.ContainsKey(possibleBehavior.BehaviorName))
+                {
+                    formsType = BehaviorGumFormsTypes[possibleBehavior.BehaviorName];
+                    behavior = possibleBehavior;
+                    break;
+                }
             }
         }
     }
@@ -1108,6 +1112,7 @@ public class CodeGenerator
         { "SliderBehavior", "MonoGameGum.Forms.Controls.Slider" },
         { "StackPanelBehavior", "MonoGameGum.Forms.Controls.StackPanel" },
         { "TextBoxBehavior", "MonoGameGum.Forms.Controls.TextBox" },
+        { "WindowBehavior", "MonoGameGum.Forms.Window" },
     };
 
     static void AddGumFormsMembers(CodeGenerationContext context)
