9edbc15532e42e5c4872ed180447b8cef07a954c
Victor Chelaru <VicChelaru@gmail.com>
2025-06-06 08:54:33 -0600

    Added FileManager unit test

---
+++ MonoGameGum.Tests/ToolsUtilities/FileManagerTests.cs
@@ -0,0 +1,25 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToolsUtilities;
+using Xunit;
+
+namespace MonoGameGum.Tests.ToolsUtilities;
+public class FileManagerTests
+{
+    [Fact]
+    public void FromFileText_ShouldLoad_WhenPathHasDotDotSlash()
+    {
+        System.IO.Directory.CreateDirectory("DirectoryA1");
+        System.IO.Directory.CreateDirectory("DirectoryB1");
+
+        System.IO.File.WriteAllText("DirectoryA1/test.txt", "Test content A");
+
+        var text = FileManager.FromFileText("DirectoryB1/../DirectoryA1/test.txt");
+
+        text.ShouldBe("Test content A");
+    }
+}
