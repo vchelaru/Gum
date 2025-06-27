7049b0202888ff3b448ae996e8b4a4d770b43dc6
Victor Chelaru <VicChelaru@gmail.com>
2025-06-26 20:47:47 -0600

    Removed TextCopy library from iOS, (#954)
    * Removed TextCopy library from iOS,
    
    Added multitargeting .NET8.0 on iOS
    
    * Installed extra workloads
    
    * More workload fixes
    
    * Removed compilation of all libraries on linux since iOS cannot be built on linux
    
    * Replaced linux with mac

--- MonoGameGum/Clipboard/ClipboardImplementation.cs
+++ MonoGameGum/Clipboard/ClipboardImplementation.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+#if !IOS
 using TextCopy;
+#endif
 
 namespace MonoGameGum.Clipboard
 {
@@ -11,12 +13,22 @@ namespace MonoGameGum.Clipboard
     {
         internal static string GetText()
         {
+#if IOS
+            return string.Empty;
+#else
             return ClipboardService.GetText();
+#endif
         }
 
         internal static void PushStringToClipboard(string text)
         {
+#if IOS
+            // todo - do we care to fix this?
+#else
+
             ClipboardService.SetText(text);
+#endif
+
         }
     }
 }
