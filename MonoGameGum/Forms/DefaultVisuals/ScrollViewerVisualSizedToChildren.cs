f1ce03ddfb47b31cb3599de2105d4b2463ea79fc
Jeremy Swartwood <jeremy.swartwood@gmail.com>
2025-07-08 23:59:06 -0800

    Create the VISUAL version of this file

---
+++ MonoGameGum/Forms/DefaultVisuals/ScrollViewerVisualSizedToChildren.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGameGum.Forms.DefaultVisuals;
+internal class ScrollViewerVisualSizedToChildren : ScrollViewerVisual
+{
+    public ScrollViewerVisualSizedToChildren(bool fullInstantiation = true, bool tryCreateFormsObject = true) : 
+        base(fullInstantiation, tryCreateFormsObject)
+    {
+        base.MakeSizedToChildren();
+    }
+}
