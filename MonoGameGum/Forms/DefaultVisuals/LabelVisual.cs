7f063747382ee10dd288c2cdc321480178f5297c
Jeremy Swartwood <jeremy.swartwood@gmail.com>
2025-07-11 17:20:38 -0800

    Convert LabelVisual to match older DefaultLabelRuntime per issue #1021

--- MonoGameGum/Forms/DefaultVisuals/LabelVisual.cs
+++ MonoGameGum/Forms/DefaultVisuals/LabelVisual.cs
@@ -5,42 +5,24 @@ using RenderingLibrary.Graphics;
 
 namespace MonoGameGum.Forms.DefaultVisuals;
 
-public class LabelVisual : InteractiveGue
+public class LabelVisual : TextRuntime
 {
-    public TextRuntime TextInstance { get; private set; }
-    public LabelVisual(bool fullInstantiation = true, bool tryCreateFormsObject = true) : base(new InvisibleRenderable())
+    public LabelVisual(bool fullInstantiation = true, bool tryCreateFormsObject = true) : base(fullInstantiation)
     {
         if (fullInstantiation)
         {
-            this.Width = 128;
-            this.WidthUnits = Gum.DataTypes.DimensionUnitType.Absolute;
-            this.Height = 0;
-            this.HeightUnits = Gum.DataTypes.DimensionUnitType.RelativeToChildren;
-            this.X = 0;
-            this.Y = 0;
-
-            TextInstance = new TextRuntime();
-            TextInstance.X = 0;
-            TextInstance.Y = 0;
-            TextInstance.Width = 0;
-            TextInstance.Height = 0;
-            TextInstance.Name = "TextInstance";
-            TextInstance.Text = "label";
-            TextInstance.WidthUnits = Gum.DataTypes.DimensionUnitType.RelativeToParent;
-            TextInstance.HeightUnits = Gum.DataTypes.DimensionUnitType.RelativeToChildren;
-            TextInstance.XOrigin = RenderingLibrary.Graphics.HorizontalAlignment.Left;
-            TextInstance.YOrigin = RenderingLibrary.Graphics.VerticalAlignment.Top;
-            TextInstance.XUnits = Gum.Converters.GeneralUnitType.PixelsFromSmall;
-            TextInstance.YUnits = Gum.Converters.GeneralUnitType.PixelsFromSmall;
-            TextInstance.HorizontalAlignment = RenderingLibrary.Graphics.HorizontalAlignment.Left;
-            TextInstance.VerticalAlignment = RenderingLibrary.Graphics.VerticalAlignment.Top;
-            this.Children.Add(TextInstance);
+            Name = "TextInstance";
+            X = 0;
+            Y = 0;
+            Width = 128;
+            WidthUnits = Gum.DataTypes.DimensionUnitType.Absolute;
+            Height = 0;
+            HeightUnits = Gum.DataTypes.DimensionUnitType.RelativeToChildren;
         }
         if (tryCreateFormsObject)
         {
             FormsControlAsObject = new Label(this);
         }
-
     }
 
     public Label FormsControl => FormsControlAsObject as Label;
