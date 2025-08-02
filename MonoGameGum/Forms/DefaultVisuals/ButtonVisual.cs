4d8dfd147e890275a6d3fab5a4e66fc53a80a11a
Jeremy Swartwood <jeremy.swartwood@gmail.com>
2025-08-01 18:39:16 -0800

    Found 2 white colors that needed to come from styling (#1165)

--- MonoGameGum/Forms/DefaultVisuals/ButtonVisual.cs
+++ MonoGameGum/Forms/DefaultVisuals/ButtonVisual.cs
@@ -79,7 +79,7 @@ public class ButtonVisual : InteractiveGue
         TextInstance.YUnits = Gum.Converters.GeneralUnitType.PixelsFromMiddle;
         TextInstance.HorizontalAlignment = HorizontalAlignment.Center;
         TextInstance.VerticalAlignment = VerticalAlignment.Center;
-        TextInstance.Color = Color.White;
+        TextInstance.Color = Styling.ActiveStyle.Colors.White;
         TextInstance.ApplyState(Styling.ActiveStyle.Text.Normal);
         this.AddChild(TextInstance);
 
