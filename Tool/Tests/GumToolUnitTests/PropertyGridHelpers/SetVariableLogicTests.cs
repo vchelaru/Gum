48828e70751e32e5eabc6d3271236a05dac9f5dd
Victor Chelaru <VicChelaru@gmail.com>
2025-08-26 07:32:01 -0600

    1332 after exposing a variable in gum for nineslice error appears (#1336)
    * Added more diagnostics on crash
    
    * fixed exposed texture coordinate values on NineSlice not having their type set.

---
+++ Tool/Tests/GumToolUnitTests/PropertyGridHelpers/SetVariableLogicTests.cs
@@ -0,0 +1,56 @@
+﻿using Gum.DataTypes;
+using Gum.Managers;
+using Gum.PropertyGridHelpers;
+using Gum.ToolStates;
+using Moq.AutoMock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GumToolUnitTests.PropertyGridHelpers;
+public class SetVariableLogicTests
+{
+    private readonly AutoMocker mocker;
+
+    private readonly SetVariableLogic _setVariableLogic;
+    public SetVariableLogicTests()
+    {
+        mocker = new();
+
+        _setVariableLogic = mocker.CreateInstance<SetVariableLogic>();
+        StandardElementsManager.Self.Initialize();
+    }
+
+    [Fact(Skip ="ProjectManager.Self needs to be removed")]
+    public void ReactToPropertyValueChanged_ShouldAddTextureAddressValues_WhenSettingTextureAddresOnNineSlice()
+    {
+        var container = new ComponentSave();
+        container.States.Add(new Gum.DataTypes.Variables.StateSave());
+        container.DefaultState.ParentContainer = container;
+        container.DefaultState.SetValue(
+            "NineSliceInstance.TextureAddress",
+            TextureAddress.Custom);
+        container.DefaultState.SetValue(
+            "NineSliceInstance.SourceFile",
+            "c:/MyFile.png");
+        
+
+        var instance = new InstanceSave();
+        instance.Name = "NineSliceInstance";
+
+        var selectedState = mocker.GetMock<ISelectedState>();
+        selectedState
+            .Setup(x => x.SelectedInstance)
+            .Returns(instance);
+
+        _setVariableLogic.ReactToPropertyValueChanged(
+            "TextureAddress",
+            TextureAddress.EntireTexture, 
+            container,
+            instance,
+            container.DefaultState,
+            refresh:true);
+    }
+}
