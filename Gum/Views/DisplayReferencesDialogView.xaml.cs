5516536364ec52940201ed40c457698edf7ba85e
vicdotexe <vicdotexe@gmail.com>
2025-09-09 23:53:41 -0700

    Migrate DisplayReferences to DialogService

---
+++ Gum/Views/DisplayReferencesDialogView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Gum.Dialogs;
+using Gum.Services.Dialogs;
+using System.Windows.Controls;
+
+namespace Gum.Views;
+
+/// <summary>
+/// Interaction logic for DisplayReferencesDialogView.xaml
+/// </summary>
+[Dialog(typeof(DisplayReferencesDialog))]
+public partial class DisplayReferencesDialogView : UserControl
+{
+    public DisplayReferencesDialogView()
+    {
+        InitializeComponent();
+    }
+}
