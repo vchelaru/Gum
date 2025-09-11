8f755b79f7d4122bac3cf2571c654313a897f938
Victor Korn <vicdotexe@gmail.com>
2025-09-10 21:40:16 -0700

    Migrate LoadRecentWindow (#1410)
    * Migrate LoadRecentWindow
    
    * allow resize and fix swallowed mousewheel events
    
    * Sneaking a small XML doc change
    
    ---------
    
    Co-authored-by: Victor Chelaru <VicChelaru@gmail.com>

--- Gum/Plugins/InternalPlugins/LoadRecentFilesPlugin/Views/LoadRecentWindow.xaml.cs
+++ Gum/Plugins/InternalPlugins/LoadRecentFilesPlugin/Views/LoadRecentWindow.xaml.cs
@@ -1,14 +1,17 @@
 ﻿using Gum.Plugins.InternalPlugins.LoadRecentFilesPlugin.ViewModels;
 using System;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using Gum.Services.Dialogs;
 
 namespace Gum.Plugins.InternalPlugins.LoadRecentFilesPlugin.Views
 {
     /// <summary>
     /// Interaction logic for LoadRecentWindow.xaml
     /// </summary>
-    public partial class LoadRecentWindow : Window
+    [Dialog(typeof(LoadRecentViewModel))]
+    public partial class LoadRecentWindow : UserControl
     {
         LoadRecentViewModel ViewModel => DataContext as LoadRecentViewModel;
 
@@ -26,48 +29,35 @@ namespace Gum.Plugins.InternalPlugins.LoadRecentFilesPlugin.Views
             //this.SearchBar.FocusTextBox();
         }
 
-        private void LoadButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (ViewModel.SelectedItem != null)
-            {
-                this.DialogResult = true;
-            }
-        }
+        //private void SearchBar_ClearSearchButtonClicked()
+        //{
+        //    ViewModel.SearchBoxText = String.Empty;
+        //}
 
-        private void CancelButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.DialogResult = false;
-        }
+        //private void SearchBar_ArrowKeyPushed(Key key)
+        //{
 
-        private void SearchBar_ClearSearchButtonClicked()
-        {
-            ViewModel.SearchBoxText = String.Empty;
-        }
+        //}
 
-        private void SearchBar_ArrowKeyPushed(Key key)
-        {
-
-        }
-
-        private void SearchBar_EnterPressed()
-        {
-            if (ViewModel.SelectedItem != null)
-            {
-                DialogResult = true;
-            }
-        }
+        //private void SearchBar_EnterPressed()
+        //{
+        //    if (ViewModel.SelectedItem != null)
+        //    {
+        //        DialogResult = true;
+        //    }
+        //}
 
         private void ListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            if (ViewModel.SelectedItem != null)
+            if (ViewModel.CanExecuteAffirmative())
             {
-                DialogResult = true;
+                ViewModel.AffirmativeCommand.Execute(null);
             }
         }
 
-        private void SearchBar_EscapePressed()
-        {
-            DialogResult = false;
-        }
+        //private void SearchBar_EscapePressed()
+        //{
+        //    DialogResult = false;
+        //}
     }
 }
