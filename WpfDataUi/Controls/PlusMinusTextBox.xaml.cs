396320f1b41a70963174402d0adfdcc1ded19fcd
Victor Korn <vicdotexe@gmail.com>
2025-09-18 04:25:34 -0700

    Refactor DataUiGrid to become an ItemsControl (#1459)
    * Refactors DataUiGrid to be an ItemsControl
    
    * remove datauiCore

--- WpfDataUi/Controls/PlusMinusTextBox.xaml.cs
+++ WpfDataUi/Controls/PlusMinusTextBox.xaml.cs
@@ -1,24 +1,11 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
+﻿
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using WpfDataUi;
-using WpfDataUi.Controls;
 using WpfDataUi.DataTypes;
 
-namespace WpfDataUiCore.Controls
+namespace WpfDataUi.Controls
 {
     /// <summary>
     /// Interaction logic for PlusMinusTextBox.xaml
---
+++ WpfDataUi/Themes/Keys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfDataUi;
+
+public static class Keys
+{
+    public static ComponentResourceKey AlternatingRowBackgroundEvenKey { get; } =
+        new ComponentResourceKey(typeof(DataUiGrid), "DataUi.AlternatingRowBackgroundEven");
+    public static ComponentResourceKey AlternatingRowBackgroundOddKey { get; } =
+        new ComponentResourceKey(typeof(DataUiGrid), "DataUi.AlternatingRowBackgroundOdd");
+}
