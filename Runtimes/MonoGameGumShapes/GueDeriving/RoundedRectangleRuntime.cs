0130546654f0d87fdc0423c2deb9091402244df7
Victor Chelaru <VicChelaru@gmail.com>
2025-09-29 21:49:02 -0600

    First pass (#1499)

---
+++ Runtimes/MonoGameGumShapes/GueDeriving/RoundedRectangleRuntime.cs
@@ -0,0 +1,79 @@
+﻿using Gum.Converters;
+using MonoGameAndGum.Renderables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGameGum.GueDeriving;
+
+public class RoundedRectangleRuntime : AposShapeRuntime
+{
+    protected override AposShapeBase ContainedRenderable => ContainedRectangle;
+
+    RoundedRectangle _containedRectangle;
+    RoundedRectangle ContainedRectangle
+    {
+        get
+        {
+            if (_containedRectangle == null)
+            {
+                _containedRectangle = this.RenderableComponent as RoundedRectangle;
+            }
+            return _containedRectangle;
+        }
+    }
+
+    public float CornerRadius
+    {
+        get => ContainedRectangle.CornerRadius;
+        set => ContainedRectangle.CornerRadius = value;
+    }
+
+    public RoundedRectangleRuntime(bool fullInstantiation = true)
+    {
+        if (fullInstantiation)
+        {
+            SetContainedObject(new RoundedRectangle());
+
+            // Make defaults 100 to match Glue
+            Width = 100;
+            Height = 100;
+
+            CornerRadius = 5;
+
+            DropshadowAlpha = 255;
+            DropshadowRed = 0;
+            DropshadowGreen = 0;
+            DropshadowBlue = 0;
+
+            DropshadowOffsetX = 0;
+            DropshadowOffsetY = 3;
+            DropshadowBlurX = 0;
+            DropshadowBlurY = 3;
+
+            //GradientType = GradientType.Linear;
+
+            GradientX1 = 0;
+            GradientX1Units = GeneralUnitType.PixelsFromSmall;
+
+            GradientY1 = 0;
+            GradientY1Units = GeneralUnitType.PixelsFromSmall;
+
+            Red1 = 255;
+            Green1 = 255;
+            Blue1 = 255;
+
+            GradientX2 = 100;
+            GradientX2Units = GeneralUnitType.PixelsFromSmall;
+
+            GradientY2 = 100;
+            GradientY2Units = GeneralUnitType.PixelsFromSmall;
+
+            Red2 = 255;
+            Green2 = 255;
+            Blue2 = 0;
+        }
+    }
+}
---
+++ Runtimes/MonoGameGumShapes/Renderables/Arc.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.Xna.Framework;
+using RenderingLibrary;
+using RenderingLibrary.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGameAndGum.Renderables;
+
+internal class Arc : AposShapeBase
+{
+
+    public float StartAngle
+    {
+        get;
+        set;
+    } = 0;
+
+    public float SweepAngle
+    {
+        get;
+        set;
+    } = 90;
+
+    public float Thickness
+    {
+        get;
+        set;
+    } = 10;
+
+    bool _isEndRounded;
+    public bool IsEndRounded
+    {
+        get => _isEndRounded;
+        set
+        {
+            _isEndRounded = value;
+        }
+    }
+
+
+
+
+    public override void Render(ISystemManagers managers)
+    {
+        if (SweepAngle == 0) return;
+
+        var sb = ShapeRenderer.ShapeBatch;
+        sb.Begin();
+
+        var absoluteLeft = this.GetAbsoluteLeft();
+        var absoluteTop = this.GetAbsoluteTop();
+
+        var center = new Microsoft.Xna.Framework.Vector2(
+            absoluteLeft + Width / 2.0f,
+            absoluteTop + Width / 2.0f);
+
+        var radius = Width / 2 - Thickness / 2;
+
+        if(HasDropshadow)
+        {
+            var shadowLeft = absoluteLeft + DropshadowOffsetX + DropshadowBlurX / 2f;
+            var shadowTop = absoluteTop + DropshadowOffsetY + DropshadowBlurX / 2f;
+
+            var dropshadowCenter = center;
+            dropshadowCenter.X += DropshadowOffsetX;
+            dropshadowCenter.Y += DropshadowOffsetY;
+
+            RenderInternal(sb, 
+                absoluteLeft: shadowLeft, 
+                absoluteTop: shadowTop, 
+                center: dropshadowCenter, 
+                radius: radius,
+                antiAliasSize: MathFunctions.RoundToInt(DropshadowBlurX),
+                lineThickness: Thickness - DropshadowOffsetX,
+                forcedColor: DropshadowColor);
+        }
+
+        RenderInternal(sb, absoluteLeft, absoluteTop, center, radius, 1, Thickness);
+
+        sb.End();
+    }
+
+    private void RenderInternal(Apos.Shapes.ShapeBatch sb, 
+        float absoluteLeft, 
+        float absoluteTop, 
+        Vector2 center, 
+        float radius,
+        int antiAliasSize, 
+        float lineThickness,
+        Color? forcedColor = null)
+    {
+        var startAngleRadians = MathHelper.ToRadians(-StartAngle);
+        float endAngleRadians = 0;
+        endAngleRadians = MathHelper.ToRadians(-StartAngle - SweepAngle);
+
+        if (startAngleRadians > endAngleRadians)
+        {
+            // swap start and end:
+            var temp = startAngleRadians;
+            startAngleRadians = endAngleRadians;
+            endAngleRadians = temp;
+        }
+
+        var endpointRadius = lineThickness / 2;
+
+        if (UseGradient && forcedColor == null)
+        {
+            var gradient = base.GetGradient(absoluteLeft, absoluteTop);
+            sb.DrawArc(center,
+                startAngleRadians,
+                endAngleRadians,
+                radius,
+                endpointRadius,
+                gradient,
+                gradient,
+                1,
+                aaSize: antiAliasSize);
+        }
+        else
+        {
+            var color = forcedColor ?? this.Color;
+            sb.DrawArc(center,
+                startAngleRadians,
+                endAngleRadians,
+                radius,
+                endpointRadius,
+                color,
+                color,
+                1,
+                aaSize: antiAliasSize);
+        }
+    }
+}
