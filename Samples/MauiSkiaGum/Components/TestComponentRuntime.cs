6e726065939d31a921db429f34fdabd5f452d80e
Victor Chelaru <VicChelaru@gmail.com>
2025-10-19 06:40:34 -0600

    1543 svg loading is broken in skiagum maui (#1597)
    * CooredCircleRuntime on skia now matches default Gum project
    
    Updated sample project to use codegen.
    
    * Embedded resource content name adjustment is now performed in SkiaResourceManager
    
    Re-initialization no longer happens per Skia view in Maui which previously wiped out the CustomResourceAssembly.
    Fixed drag+drop of file doing a to-lower.

---
+++ Samples/MauiSkiaGum/Components/TestComponentRuntime.cs
@@ -0,0 +1,19 @@
+using Gum.Converters;
+using Gum.DataTypes;
+using Gum.Managers;
+using Gum.Wireframe;
+
+using RenderingLibrary.Graphics;
+
+using System.Linq;
+
+namespace MauiSkiaGum.Components
+{
+    partial class TestComponentRuntime
+    {
+        partial void CustomInitialize()
+        {
+        
+        }
+    }
+}
