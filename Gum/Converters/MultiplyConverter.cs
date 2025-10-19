db03cce37f050d37abd02955617a94cec486203d
Victor Chelaru <VicChelaru@gmail.com>
2025-10-19 13:12:03 -0600

    Pushed diagnostics

--- Gum/Converters/MultiplyConverter.cs
+++ Gum/Converters/MultiplyConverter.cs
@@ -8,9 +8,32 @@ public class MultiplyConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
+
         if (value is double d && parameter is string s && double.TryParse(s, out double factor))
+        {
             return d * factor;
-        return Math.Round((decimal)value);
+
+        }
+        else
+        {
+            if(value is double)
+            {
+                var isParameterString = false;
+                bool? canParse = null;
+                if(parameter is string asString)
+                {
+                    isParameterString = true;
+                    canParse = false;
+                    if(isParameterString)
+                    {
+                        canParse = double.TryParse(asString, out _);
+                    }
+                }
+
+                int m = 3;
+            }
+            return Math.Round((decimal)value);
+        }
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
