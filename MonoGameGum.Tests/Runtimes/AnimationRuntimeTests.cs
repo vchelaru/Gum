13bfe8ec12ed504bdedfac7f38f1fcb4ca8f3055
Victor Chelaru <VicChelaru@gmail.com>
2025-11-01 07:45:58 -0600

    Added unit tests
    Refactored VariableSave

--- MonoGameGum.Tests/Runtimes/AnimationRuntimeTests.cs
+++ MonoGameGum.Tests/Runtimes/AnimationRuntimeTests.cs
@@ -64,6 +64,94 @@ public class AnimationRuntimeTests : BaseTestClass
     }
 
     [Fact]
+    public void RefreshCumulativeStates_ShouldInterpolateValuesPoperly()
+    {
+        ComponentSave element = new();
+        element.States.Add(new StateSave()); // give it a default state
+        element.Name = "Animated component";
+
+        StateSaveCategory alphaCategory = new();
+        element.Categories.Add(alphaCategory);
+        alphaCategory.Name = "AlphaCategory";
+
+        StateSave alphaState1 = new();
+        alphaCategory.States.Add(alphaState1);
+        alphaState1.Name = "AlphaState1";
+        alphaState1.Variables.Add(new VariableSave()
+        {
+            Name = "Alpha",
+            Value = 0f
+        });
+        
+        StateSave alphaState2 = new();
+        alphaCategory.States.Add(alphaState2);
+        alphaState2.Name = "AlphaState2";
+        alphaState2.Variables.Add(new VariableSave()
+        {
+            Name = "Alpha",
+            Value = 1f
+        });
+
+        StateSaveCategory positionCategory = new();
+        element.Categories.Add(positionCategory);
+        positionCategory.Name = "PositionCategory";
+
+        StateSave positionState1 = new();
+        positionCategory.States.Add(positionState1);
+        positionState1.Name = "PositionState1";
+        positionState1.Variables.Add(new VariableSave()
+        {
+            Name = "X",
+            Value = 0f
+        });
+
+        StateSave positionState2 = new();
+        positionCategory.States.Add(positionState2);
+        positionState2.Name = "PositionState2";
+        positionState2.Variables.Add(new VariableSave()
+        {
+            Name = "X",
+            Value = 100f
+        });
+
+        KeyframeRuntime alphaKeyframe1 = new();
+        alphaKeyframe1.InterpolationType = FlatRedBall.Glue.StateInterpolation.InterpolationType.Linear;
+        alphaKeyframe1.Time = 0;
+        alphaKeyframe1.StateName = "AlphaCategory/AlphaState1";
+
+        KeyframeRuntime alphaKeyframe2 = new();
+        alphaKeyframe2.Time = 1;
+        alphaKeyframe2.StateName = "AlphaCategory/AlphaState2";
+
+        KeyframeRuntime positionKeyframe1 = new();
+        positionKeyframe1.InterpolationType = FlatRedBall.Glue.StateInterpolation.InterpolationType.Linear;
+        positionKeyframe1.Time = 0.5f;
+        positionKeyframe1.StateName = "PositionCategory/PositionState1";
+
+        KeyframeRuntime positionKeyframe2 = new();
+        positionKeyframe2.Time = 1.5f;
+        positionKeyframe2.StateName = "PositionCategory/PositionState2";
+
+        AnimationRuntime animation = new();
+        animation.Keyframes.Add(alphaKeyframe1);
+        animation.Keyframes.Add(positionKeyframe1);
+        animation.Keyframes.Add(alphaKeyframe2);
+        animation.Keyframes.Add(positionKeyframe2);
+
+        animation.RefreshCumulativeStates(element);
+
+        StateSave at0_5 = animation.GetStateToSet(0.5f, element, defaultIfNull: true);
+
+        at0_5.Variables.First(item => item.Name == "Alpha").Value.ShouldBe(0.5f);
+
+        StateSave at1_0 = animation.GetStateToSet(1.0f, element, defaultIfNull: true);
+
+        at1_0.Variables.First(item => item.Name == "Alpha").Value.ShouldBe(1f);
+        at1_0.Variables.First(item => item.Name == "X").Value.ShouldBe(50f);
+    }
+
+
+    [Fact]
     public void GetStateToSet_ShouldThrowException_IfRefreshCmulativeStatesIsntCalled()
     {
         ComponentSave element = new();
