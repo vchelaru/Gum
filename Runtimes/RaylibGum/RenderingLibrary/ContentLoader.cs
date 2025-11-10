3ac21462f8c5753ed116025fcbd6e679fa5915ac
Victor Chelaru <VicChelaru@gmail.com>
2025-11-10 06:22:52 -0700

    raylib conternt loading now caches textures, preventing multiple re-loads of the same texture (#1716)

--- Runtimes/RaylibGum/RenderingLibrary/ContentLoader.cs
+++ Runtimes/RaylibGum/RenderingLibrary/ContentLoader.cs
@@ -1,10 +1,12 @@
 ﻿using Raylib_cs;
+using RenderingLibrary;
 using RenderingLibrary.Content;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToolsUtilities;
 using static Raylib_cs.Raylib;
 
 namespace RenderingLibrary.Content;
@@ -15,10 +17,7 @@ internal class ContentLoader : IContentLoader
     {
         if (typeof(T) == typeof(Texture2D))
         {
-            Image image = LoadImage(contentName);
-
-            //Transform it as a texture
-            return (T)(object)LoadTextureFromImage(image);
+            return (T)(object)LoadTexture2D(contentName);
         }
         else if(typeof(T) == typeof(Font))
         {
@@ -42,6 +41,65 @@ internal class ContentLoader : IContentLoader
             throw new NotImplementedException($"Error attempting to load {contentName} of type {typeof(T).AssemblyQualifiedName}");
         }
     }
+
+    private static Texture2D? LoadTexture2D(string fileName)
+    {
+        string fileNameStandardized = StandardizeCaseSensitive(fileName);
+
+        Texture2D? toReturn = null;
+
+        if (LoaderManager.Self.CacheTextures)
+        {
+            var cached = LoaderManager.Self.GetDisposable(fileNameStandardized) as ManagedTexture;
+            if (cached != null)
+            {
+                return cached.Texture;
+            }
+        }
+
+        if (FileManager.IsUrl(fileName))
+        {
+            throw new NotImplementedException("Loading textures from URLs is not implemented yet.");
+        }
+        else
+        {
+            toReturn = LoadTextureFromFile(fileName);
+        }
+
+        if (LoaderManager.Self.CacheTextures && toReturn != null)
+        {
+            var managedTexture = new ManagedTexture(toReturn.Value);
+
+            LoaderManager.Self.AddDisposable(fileNameStandardized, managedTexture);
+        }
+
+        return toReturn;
+    }
+
+    private static Texture2D LoadTextureFromFile(string fileName)
+    {
+        Image image = LoadImage(fileName);
+        //Transform it as a texture
+        var toReturn = LoadTextureFromImage(image);
+        return toReturn;
+    }
+
+    public static string StandardizeCaseSensitive(string fileName)
+    {
+        const bool preserveCase = true;
+
+        string fileNameStandardized = FileManager.Standardize(fileName, preserveCase, false);
+
+        if (FileManager.IsRelative(fileNameStandardized) && FileManager.IsUrl(fileName) == false)
+        {
+            fileNameStandardized = FileManager.RelativeDirectory + fileNameStandardized;
+
+            fileNameStandardized = FileManager.RemoveDotDotSlash(fileNameStandardized);
+        }
+
+        return fileNameStandardized;
+    }
+
     string GetSystemFontPath(string fontFileName)
     {
         if(fontFileName.EndsWith(".ttf") == false)
---
+++ Runtimes/RaylibGum/RenderingLibrary/ManagedTexture2D.cs
@@ -0,0 +1,31 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenderingLibrary;
+
+/// <summary>
+/// Wrapper of raylib Texture2D that implements IDisposable for proper resource management.
+/// </summary>
+public class ManagedTexture : IDisposable
+{
+    public Texture2D Texture { get; private set; }
+    private bool disposed = false;
+
+    public ManagedTexture(Texture2D texture)
+    {
+        Texture = texture;
+    }
+
+    public void Dispose()
+    {
+        if (!disposed)
+        {
+            Raylib.UnloadTexture(Texture);
+            disposed = true;
+        }
+    }
+}
