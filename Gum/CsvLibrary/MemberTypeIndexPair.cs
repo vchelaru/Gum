fa960254e8cd799ecb6fabfb45a3e065ff1f3986
Victor Chelaru <VicChelaru@gmail.com>
2025-11-21 16:01:40 -0700

    Moved CommonFormsAndControls
    Moved CsvLibrary

---
+++ Gum/CsvLibrary/MemberTypeIndexPair.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+
+namespace CsvLibrary
+{
+
+    internal struct MemberTypeIndexPair
+    {
+        public MemberTypes MemberType;
+        public int Index;
+
+    }   
+}
