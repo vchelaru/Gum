e715cbe9096a41a7932d7164631fd05f5fd21ba0
Victor Chelaru <VicChelaru@gmail.com>
2025-11-22 04:51:38 -0700

    Moved files

---
+++ Gum/StateAnimationPlugin/Views/SubAnimationSelectionWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Gum.Services.Dialogs;
+using StateAnimationPlugin.ViewModels;
+using System.Windows.Controls;
+
+namespace StateAnimationPlugin.Views;
+
+/// <summary>
+/// Interaction logic for SubAnimationSelectionWindow.xaml
+/// </summary>
+[Dialog(typeof(SubAnimationSelectionDialogViewModel))]
+public partial class SubAnimationSelectionWindow : UserControl
+{
+    public SubAnimationSelectionWindow()
+    {
+        InitializeComponent();
+    }
+
+}
