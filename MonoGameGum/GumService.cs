1a6f17c6b1c19c1852142d45169d0e599a2001b5
Victor Chelaru <VicChelaru@gmail.com>
2025-12-07 10:15:52 -0700

    Added GumService.IsInitialized (#1846)
    Unified more code between raylib and monogame in GumService.

--- MonoGameGum/GumService.cs
+++ MonoGameGum/GumService.cs
@@ -13,17 +13,16 @@ using RenderingLibrary.Content;
 using RenderingLibrary.Graphics;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 using ToolsUtilities;
 using Gum.Forms;
-using System.Collections.Specialized;
 using Gum.Threading;
 
+#if MONOGAME || KNI || FNA
 namespace MonoGameGum;
+#elif RAYLIB
+namespace RaylibGum;
+#endif
 
 public class GumService
 {
@@ -43,11 +42,13 @@ public class GumService
 
     #endregion
 
+#if MONOGAME || KNI || FNA
     public GameTime GameTime { get; private set; }
+#endif
 
-    public Cursor Cursor => Gum.Forms.FormsUtilities.Cursor;
+    public Cursor Cursor => FormsUtilities.Cursor;
 
-    public Keyboard Keyboard => Gum.Forms.FormsUtilities.Keyboard;
+    public Keyboard Keyboard => FormsUtilities.Keyboard;
 
     public GamePad[] Gamepads => Gum.Forms.FormsUtilities.Gamepads;
 
@@ -179,17 +180,17 @@ public class GumService
         return InitializeInternal(null, graphicsDevice, gumProjectFile);
     }
 
-    bool hasBeenInitialized = false;
+    public bool IsInitialized { get; private set; }
     GumProjectSave? InitializeInternal(Game game, GraphicsDevice graphicsDevice, 
         string? gumProjectFile = null, 
         SystemManagers? systemManagers = null, 
         Gum.Forms.DefaultVisualsVersion defaultVisualsVersion = Gum.Forms.DefaultVisualsVersion.V1)
     {
-        if(hasBeenInitialized)
+        if(IsInitialized)
         {
             throw new InvalidOperationException("Initialize has already been called once. It cannot be called again");
         }
-        hasBeenInitialized = true;
+        IsInitialized = true;
 
         _game = game;
         RegisterRuntimeTypesThroughReflection();
@@ -202,8 +203,13 @@ public class GumService
             ISystemManagers.Default = this.SystemManagers;
 #endif
         }
+
+#if MONOGAME || FNA || KNI
         this.SystemManagers.Initialize(graphicsDevice, fullInstantiation: true);
-        
+#elif raylib
+
+#endif
+
         FormsUtilities.InitializeDefaults(systemManagers: this.SystemManagers, defaultVisualsVersion: defaultVisualsVersion);
 
 
@@ -213,9 +219,6 @@ public class GumService
         var mainLayer = SystemManagers.Renderer.MainLayer;
         mainLayer.Remove(Root.RenderableComponent as IRenderableIpso);
         mainLayer.Insert(0, Root.RenderableComponent as IRenderableIpso);
-
-        // make sure the Root is the first in the list:
-
 
         GumProjectSave? gumProject = null;
 
@@ -283,7 +286,7 @@ public class GumService
         }
     }
 
-    #endregion
+#endregion
 
     #region Update
 
