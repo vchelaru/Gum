d93f2770bec6f3d4e10667f684875589bbc0f547
Victor Chelaru <VicChelaru@gmail.com>
2026-01-05 07:54:24 -0700

    Added camera unit tests

---
+++ MonoGameGum.Tests/RenderingLibraries/CameraTests.cs
@@ -0,0 +1,58 @@
+﻿using RenderingLibrary;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MonoGameGum.Tests.RenderingLibraries;
+
+public class CameraTests : BaseTestClass
+{
+    [Fact]
+    public void ScreenToWorld_ShouldRoundTripBackCorrectly()
+    {
+        Camera _sut = new Camera();
+        _sut.ClientWidth = 800;
+        _sut.ClientHeight = 600;
+
+        _sut.ScreenToWorld(100, 150, out float worldX, out float worldY);
+
+        _sut.WorldToScreen(worldX, worldY, out float screenX, out float screenY);
+
+        screenX.ShouldBe(100);
+        screenY.ShouldBe(150);
+    }
+
+    [Fact]
+    public void ScreenToWorld_WithZoom_ShouldRoundTripBackCorrectly()
+    {
+        Camera _sut = new Camera();
+        _sut.ClientWidth = 800;
+        _sut.ClientHeight = 600;
+        _sut.Zoom = 2;
+
+        _sut.ScreenToWorld(100, 150, out float worldX, out float worldY);
+
+        _sut.WorldToScreen(worldX, worldY, out float screenX, out float screenY);
+
+        screenX.ShouldBe(100);
+        screenY.ShouldBe(150);
+    }
+
+    [Fact]
+    public void ScreenToWorld_WithClientLeftTop_ShouldRoundTripBackCorrectly()
+    {
+        Camera _sut = new Camera();
+        _sut.ClientWidth = 800;
+        _sut.ClientHeight = 600;
+        _sut.ClientLeft = 50;
+        _sut.ClientTop = 75;
+        _sut.ScreenToWorld(100, 150, out float worldX, out float worldY);
+        _sut.WorldToScreen(worldX, worldY, out float screenX, out float screenY);
+        screenX.ShouldBe(100);
+        screenY.ShouldBe(150);
+    }
+}
