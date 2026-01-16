4a3b81f9c05eaffa9bf6163f5a58c76b41101ceb
Yuechen Li <yuechenli@outlook.com>
2026-01-16 00:31:54 -0800

    Separate Enums/Internal Classes.

---
+++ GumCommon/GraphicalUIElement/Structure/GraphicalUiElement.Enums.cs
@@ -0,0 +1,43 @@
+﻿using Gum.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gum.Wireframe
+{
+    public partial class GraphicalUiElement
+    {
+        #region Enums/Internal Classes
+
+        enum ChildType
+        {
+            Absolute = 1,
+            Relative = 1 << 1,
+            BothAbsoluteAndRelative = Absolute | Relative,
+            StackedWrapped = 1 << 2,
+            All = Absolute | Relative | StackedWrapped
+        }
+
+        class DirtyState
+        {
+            public ParentUpdateType ParentUpdateType;
+            public int ChildrenUpdateDepth;
+            public XOrY? XOrY;
+        }
+
+        public enum ParentUpdateType
+        {
+            None = 0,
+            IfParentStacks = 1,
+            IfParentWidthHeightDependOnChildren = 2,
+            IfParentIsAutoGrid = 4,
+            IfParentHasRatioSizedChildren = 8,
+            All = 16
+
+        }
+
+        #endregion
+    }
+}
--- GumRuntime/GraphicalUiElement.cs
+++ GumRuntime/GraphicalUiElement.cs
@@ -76,36 +76,7 @@ public enum Dock
 /// </summary>
 public partial class GraphicalUiElement : IRenderableIpso, IVisible, INotifyPropertyChanged
 {
-    #region Enums/Internal Classes
 
-    enum ChildType
-    {
-        Absolute = 1,
-        Relative = 1 << 1,
-        BothAbsoluteAndRelative = Absolute | Relative,
-        StackedWrapped = 1 << 2,
-        All = Absolute | Relative | StackedWrapped
-    }
-
-    class DirtyState
-    {
-        public ParentUpdateType ParentUpdateType;
-        public int ChildrenUpdateDepth;
-        public XOrY? XOrY;
-    }
-
-    public enum ParentUpdateType
-    {
-        None = 0,
-        IfParentStacks = 1,
-        IfParentWidthHeightDependOnChildren = 2,
-        IfParentIsAutoGrid = 4,
-        IfParentHasRatioSizedChildren = 8,
-        All = 16
-
-    }
-
-    #endregion
 
     #region Fields
 
