2819457d62fc41ec9116686df65850b2fe243b3f
Victor Chelaru <VicChelaru@gmail.com>
2026-01-29 05:25:40 -0700

    General reorganization and refactoring

--- Tool/EditorTabPlugin_XNA/ExtensionMethods/CursorExtensionMethods.cs
+++ Tool/EditorTabPlugin_XNA/ExtensionMethods/CursorExtensionMethods.cs
@@ -2,44 +2,41 @@
 using RenderingLibrary.Graphics;
 using RenderingLibrary;
 
-namespace Gum.Input
+namespace Gum.Input;
+
+public static class CursorExtensionMethods
 {
-    public static class CursorExtensionMethods
+    public static float GetWorldX(this Cursor cursor, SystemManagers? managers = null)
     {
-        public static float GetWorldX(this Cursor cursor, SystemManagers managers = null)
+        Renderer? renderer = null;
+        if (managers == null)
         {
-            Renderer renderer = null;
-            if (managers == null)
-            {
-                renderer = Renderer.Self;
-            }
-            else
-            {
-                renderer = managers.Renderer;
-            }
-
-            float worldX, worldY;
-            renderer.Camera.ScreenToWorld(cursor.X, cursor.Y, out worldX, out worldY);
-
-            return worldX;
+            renderer = Renderer.Self;
+        }
+        else
+        {
+            renderer = managers.Renderer;
         }
 
-        public static float GetWorldY(this Cursor cursor, SystemManagers managers = null)
+        renderer.Camera.ScreenToWorld(cursor.X, cursor.Y, out float worldX, out float worldY);
+
+        return worldX;
+    }
+
+    public static float GetWorldY(this Cursor cursor, SystemManagers? managers = null)
+    {
+        Renderer? renderer = null;
+        if (managers == null)
         {
-            Renderer renderer = null;
-            if (managers == null)
-            {
-                renderer = Renderer.Self;
-            }
-            else
-            {
-                renderer = managers.Renderer;
-            }
-
-            float worldX, worldY;
-            renderer.Camera.ScreenToWorld(cursor.X, cursor.Y, out worldX, out worldY);
-
-            return worldY;
+            renderer = Renderer.Self;
         }
+        else
+        {
+            renderer = managers.Renderer;
+        }
+
+        renderer.Camera.ScreenToWorld(cursor.X, cursor.Y, out float worldX, out float worldY);
+
+        return worldY;
     }
 }
