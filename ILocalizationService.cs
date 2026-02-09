5c66195f2b37aad8ee1437ee6d556eae7731c01d
Victor Chelaru <VicChelaru@gmail.com>
2026-02-09 08:54:17 -0700

    Fix FRB localization namespace

--- ILocalizationService.cs
+++ ILocalizationService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MonoGameGum.Localization;
+namespace Gum.Localization;
 public interface ILocalizationService
 {
     string Translate(string stringId);
