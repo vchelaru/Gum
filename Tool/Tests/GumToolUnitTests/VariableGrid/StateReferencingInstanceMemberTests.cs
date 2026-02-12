14cd14cf6f32bf7ff2883af11c85ea758368ab45
Victor Chelaru <VicChelaru@gmail.com>
2026-02-12 07:21:48 -0700

    Fixed unit tests

--- Tool/Tests/GumToolUnitTests/VariableGrid/StateReferencingInstanceMemberTests.cs
+++ Tool/Tests/GumToolUnitTests/VariableGrid/StateReferencingInstanceMemberTests.cs
@@ -1,4 +1,6 @@
-﻿using Gum.Commands;
+﻿using FlatRedBall.Glue.StateInterpolation;
+using Gum.Commands;
+using Gum.DataTypes;
 using Gum.DataTypes.Variables;
 using Gum.Managers;
 using Gum.Plugins.InternalPlugins.VariableGrid;
@@ -7,10 +9,12 @@ using Gum.Services;
 using Gum.ToolStates;
 using Gum.Undo;
 using Gum.Wireframe;
+using Moq;
 using Moq.AutoMock;
 using Shouldly;
 using System;
 using System.ComponentModel;
+using ToolsUtilities;
 
 namespace GumToolUnitTests.VariableGrid;
 
@@ -18,18 +22,22 @@ public class StateReferencingInstanceMemberTests
 {
     private readonly AutoMocker _mocker;
 
+    StateReferencingInstanceMember _sut;
+
     public StateReferencingInstanceMemberTests()
     {
         _mocker = new();
-    }
-
-    [Fact]
-    public void SetValue_ShouldStoreLastValue()
-    {
         StateSave stateSave = new StateSave();
         stateSave.SetValue("testVariableName", 3);
 
-        var sut = new StateReferencingInstanceMember(
+        Mock<ISetVariableLogic> setVariableLogic = _mocker.GetMock<ISetVariableLogic>();
+        setVariableLogic
+            .Setup(x => x.PropertyValueChanged(It.IsAny<string>(), It.IsAny<object?>(),
+                It.IsAny<InstanceSave>(), It.IsAny<StateSave>() , It.IsAny<bool>() , It.IsAny<bool>(),
+                It.IsAny<bool>()))
+            .Returns(GeneralResponse.SuccessfulResponse);
+
+        _sut = new StateReferencingInstanceMember(
             new Attribute[0],
             null,
             typeof(int),
@@ -49,10 +57,23 @@ public class StateReferencingInstanceMemberTests
             _mocker.Get<ISelectedState>(),
             _mocker.Get<IGuiCommands>(),
             _mocker.Get<IFileCommands>(),
-            _mocker.Get<ISetVariableLogic>(),
+            setVariableLogic.Object,
             _mocker.Get<WireframeObjectManager>());
+    }
 
-        sut.SetValue(1, WpfDataUi.DataTypes.SetPropertyCommitType.Full);
-        sut.LastOldFullCommitValue.ShouldBe(3);
+    [Fact]
+    public void SetValue_ShouldStoreLastValue()
+    {
+
+        ComponentSave componentSave = new ComponentSave();
+        componentSave.States.Add(new StateSave
+        {
+
+        });
+
+        _sut.Instance = componentSave;
+
+        _sut.SetValue(1, WpfDataUi.DataTypes.SetPropertyCommitType.Full);
+        _sut.LastOldFullCommitValue.ShouldBe(3);
     }
 }
