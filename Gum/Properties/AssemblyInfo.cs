6a239a570affba57d4d29b54d2cccfe3701f9d3e
vchelaru <729631+vchelaru@users.noreply.github.com>
2026-08-04 15:05:49 +0000

    [create-pull-request] automated change

--- Gum/Properties/AssemblyInfo.cs
+++ Gum/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // BuildVersion is the source of truth for the tool's displayed version (see
 // MenuStripManager's About dialog). Kept as an ordinary AssemblyMetadata attribute rather
 // than AssemblyVersion/AssemblyFileVersion - see the comment above.
-[assembly: AssemblyMetadata("BuildVersion", "2026.08.03")]
+[assembly: AssemblyMetadata("BuildVersion", "2026.08.04")]
 [assembly: InternalsVisibleTo("GumToolUnitTests")]
 [assembly: InternalsVisibleTo("EditorTabPlugin_XNA")]
